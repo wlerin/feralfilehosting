96842149fd53b9aa232b41fdf1c6b0a766fd8af7
randomessence <random@digikool.co.uk>
2013-10-10 10:18:09 +0100

    autodl

--- Feral Wiki/Software/Autodl-irssi and rutorrent plugin - community edition/scripts/installautodl.sh
+++ Feral Wiki/Software/Autodl-irssi and rutorrent plugin - community edition/scripts/installautodl.sh
@@ -42,7 +42,7 @@ port=$(shuf -i 6000-50000 -n 1)
 # sets of two new variables to make things easier in the echo
 autodlPort='$autodlPort'
 autodlPassword='$autodlPassword'
-pass=$(< /dev/urandom tr -dc '12345!@#ANCDEFGHIJKLMNOPabcdefghijklmnop' | head -c${1:-15};echo;)
+pass=$(< /dev/urandom tr -dc '12345!@#ANCDEFGHIJKLMNOPabcdefghijklmnop' | head -c${1:-20};echo;)
 #
 ############################
 ####### Variable End #######
