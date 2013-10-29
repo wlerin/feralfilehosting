47bc2ef5558a8a1dec0fba8d23a0db589cd50e36
randomessence <random@digikool.co.uk>
2013-10-29 22:35:44 +0000

    multi

--- Feral Wiki/Software/Git - How to Install/readme.md
+++ Feral Wiki/Software/Git - How to Install/readme.md
@@ -7,9 +7,9 @@ This command will show you your installed Git version
 git --version
 ~~~
 
-If you specifically need a higher version you can used the FAQ to install 1.8.3.1.
+If you specifically need a higher version you can used the FAQ to install 1.8.4
 
-### Git 1.8.4.0
+### Git 1.8.4.2
 
 You need to SSH into your slot to complete this guide. If you don't know how to do this [here is a basic guide](https://www.feralhosting.com/faq/view?question=12)
 
@@ -19,10 +19,10 @@ mkdir -p ~/programs
 ~~~
 
 ~~~
-wget -qO ~/git-1.8.4.tar.gz  http://git-core.googlecode.com/files/git-1.8.4.tar.gz
-tar xf ~/git-1.8.4.tar.gz && cd ~/git-1.8.4
+wget -qO ~/git-1.8.4.2.tar.gz http://git-core.googlecode.com/files/git-1.8.4.2.tar.gz
+tar xf ~/git-1.8.4.2.tar.gz && cd ~/git-1.8.4.2
 ./configure --prefix=$HOME/programs --with-curl=/opt/curl/current
-make && make install && cd && rm -rf ~/git-1.8.4 git-1.8.4.tar.gz
+make && make install && cd && rm -rf ~/git-1.8.4.2 git-1.8.4.2.tar.gz
 ~~~
 
 Then do this to check the version.
--- Feral Wiki/Software/Multiple instances - rtorrent and rutorrent/script/multirtru.sh
+++ Feral Wiki/Software/Multiple instances - rtorrent and rutorrent/script/multirtru.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # multi rtorrent/rutorrent
-scriptversion="1.0.3"
+scriptversion="1.0.4"
 scriptname="multirtru"
 # Author name
 #
@@ -40,18 +40,18 @@ scriptname="multirtru"
 #
 mkdir -p $HOME/bin
 #
-if [ ! -f $HOME/multirtru.sh ]
+if [[ ! -f "$HOME/multirtru.sh" ]]
 then
     wget -qO $HOME/multirtru.sh https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Multiple%20instances%20-%20rtorrent%20and%20rutorrent/script/multirtru.sh
 fi
-if [ ! -f $HOME/bin/multirtru ]
+if [[ ! -f "$HOME/bin/multirtru" ]]
 then
     wget -qO $HOME/bin/multirtru https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Multiple%20instances%20-%20rtorrent%20and%20rutorrent/script/multirtru.sh
 fi
 #
 wget -qO $HOME/000multirtru.sh https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Multiple%20instances%20-%20rtorrent%20and%20rutorrent/script/multirtru.sh
 #
-if ! diff -q $HOME/000multirtru.sh $HOME/multirtru.sh > /dev/null 2>&1
+if ! diff -q "$HOME/000multirtru.sh" "$HOME/multirtru.sh" > /dev/null 2>&1
 then
     echo '#!/bin/bash
     wget -qO $HOME/multirtru.sh https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Multiple%20instances%20-%20rtorrent%20and%20rutorrent/script/multirtru.sh
@@ -61,7 +61,7 @@ then
     bash $HOME/111multirtru.sh
     exit 1
 fi
-if ! diff -q $HOME/000multirtru.sh $HOME/bin/multirtru > /dev/null 2>&1
+if ! diff -q "$HOME/000multirtru.sh" "$HOME/bin/multirtru" > /dev/null 2>&1
 then
     echo '#!/bin/bash
     wget -qO $HOME/multirtru.sh https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Multiple%20instances%20-%20rtorrent%20and%20rutorrent/script/multirtru.sh
