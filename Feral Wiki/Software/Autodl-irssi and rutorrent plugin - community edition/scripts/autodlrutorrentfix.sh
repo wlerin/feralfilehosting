d0888aadb5bcab62e419a5bc337119d525f77fd2
randomessence <random@digikool.co.uk>
2013-12-24 21:50:54 +0000

    autodl

--- Feral Wiki/Software/Autodl-irssi and rutorrent plugin - community edition/scripts/autodlrutorrentfix.sh
+++ Feral Wiki/Software/Autodl-irssi and rutorrent plugin - community edition/scripts/autodlrutorrentfix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # autodlrutorrentfix.sh
-scriptversion="1.0.3"
+scriptversion="1.0.4"
 scriptname="autodlrutorrentfix"
 # randomessence
 #
@@ -10,14 +10,7 @@ scriptname="autodlrutorrentfix"
 ## Version History Starts ##
 ############################
 #
-# How do I customise this updater? 
-# 1: scriptversion="0.0.0" replace "0.0.0" with your script version. This will be shown to the user at the current version.
-# 2: scriptname="autodlrutorrentfix" replace "autodlrutorrentfix" with your script name. this will be shown to the user when they first run the script.
-# 3: Search and replace all instances of "autodlrutorrentfix", 29 including this one, with the name of your script, do not include the .sh aside from doing step 2.
-# 4: Then replace ALL "https://raw.github.com/feralhosting/feralfilehosting/master/Feral" with the URL to the RAW script URL.
-# 5: Insert you script in the "Script goes here" labelled section 
-#
-# This updater deals with updating two files at the same time, the  "~/autodlrutorrentfix.sh" and the "~/bin/autodlrutorrentfix" . You can remove one part of the updater, if you wish, to focus on a single file instance.
+# 1.0.4 modernised.
 #
 ############################
 ### Version History Ends ###
@@ -28,7 +21,7 @@ scriptname="autodlrutorrentfix"
 ###### Variable Start ######
 ############################
 #
-#
+scripturl="https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Autodl-irssi%20and%20rutorrent%20plugin%20-%20community%20edition/scripts/autodlrutorrentfix.sh"
 #
 ############################
 ####### Variable End #######
@@ -38,37 +31,37 @@ scriptname="autodlrutorrentfix"
 #### Self Updater Start ####
 ############################
 #
-mkdir -p $HOME/bin
+mkdir -p "$HOME/bin"
 #
-if [ ! -f $HOME/autodlrutorrentfix.sh ]
+if [[ ! -f "$HOME/autodlrutorrentfix.sh" ]]
 then
-    wget -qO $HOME/autodlrutorrentfix.sh https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Autodl-irssi%20and%20rutorrent%20plugin%20-%20community%20edition/scripts/autodlrutorrentfix.sh
+    wget -qO "$HOME/autodlrutorrentfix.sh" "$scripturl"
 fi
-if [ ! -f $HOME/bin/autodlrutorrentfix ]
+if [[ ! -f "$HOME/bin/autodlrutorrentfix" ]]
 then
-    wget -qO $HOME/bin/autodlrutorrentfix https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Autodl-irssi%20and%20rutorrent%20plugin%20-%20community%20edition/scripts/autodlrutorrentfix.sh
+    wget -qO "$HOME/bin/autodlrutorrentfix" "$scripturl"
 fi
 #
-wget -qO $HOME/000autodlrutorrentfix.sh https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Autodl-irssi%20and%20rutorrent%20plugin%20-%20community%20edition/scripts/autodlrutorrentfix.sh
+wget -qO "$HOME/000autodlrutorrentfix.sh" "$scripturl"
 #
-if ! diff -q $HOME/000autodlrutorrentfix.sh $HOME/autodlrutorrentfix.sh > /dev/null 2>&1
+if ! diff -q "$HOME/000autodlrutorrentfix.sh" "$HOME/autodlrutorrentfix.sh" > /dev/null 2>&1
 then
     echo '#!/bin/bash
-    wget -qO $HOME/autodlrutorrentfix.sh https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Autodl-irssi%20and%20rutorrent%20plugin%20-%20community%20edition/scripts/autodlrutorrentfix.sh
-    wget -qO $HOME/bin/autodlrutorrentfix https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Autodl-irssi%20and%20rutorrent%20plugin%20-%20community%20edition/scripts/autodlrutorrentfix.sh
-    bash $HOME/autodlrutorrentfix.sh
-    exit 1' > $HOME/111autodlrutorrentfix.sh
-    bash $HOME/111autodlrutorrentfix.sh
+    wget -qO "$HOME/autodlrutorrentfix.sh" "'"$scripturl"'"
+    wget -qO "$HOME/bin/autodlrutorrentfix" "'"$scripturl"'"
+    bash "$HOME/autodlrutorrentfix.sh"
+    exit 1' > "$HOME/111autodlrutorrentfix.sh"
+    bash "$HOME/111autodlrutorrentfix.sh"
     exit 1
 fi
-if ! diff -q $HOME/000autodlrutorrentfix.sh $HOME/bin/autodlrutorrentfix > /dev/null 2>&1
+if ! diff -q "$HOME/000autodlrutorrentfix.sh" "$HOME/bin/autodlrutorrentfix" > /dev/null 2>&1
 then
     echo '#!/bin/bash
-    wget -qO $HOME/autodlrutorrentfix.sh https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Autodl-irssi%20and%20rutorrent%20plugin%20-%20community%20edition/scripts/autodlrutorrentfix.sh
-    wget -qO $HOME/bin/autodlrutorrentfix https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Autodl-irssi%20and%20rutorrent%20plugin%20-%20community%20edition/scripts/autodlrutorrentfix.sh
-    bash $HOME/autodlrutorrentfix.sh
-    exit 1' > $HOME/222autodlrutorrentfix.sh
-    bash $HOME/222autodlrutorrentfix.sh
+    wget -qO "$HOME/autodlrutorrentfix.sh" "'"$scripturl"'"
+    wget -qO "$HOME/bin/autodlrutorrentfix" "'"$scripturl"'"
+    bash "$HOME/autodlrutorrentfix.sh"
+    exit 1' > "$HOME/222autodlrutorrentfix.sh"
+    bash "$HOME/222autodlrutorrentfix.sh"
     exit 1
 fi
 #
@@ -76,8 +69,8 @@ echo
 echo -e "Hello $(whoami), you have the latest version of the" "\033[36m""$scriptname""\e[0m" "script. This script version is:" "\033[31m""$scriptversion""\e[0m"
 echo
 #
-rm -f $HOME/000autodlrutorrentfix.sh $HOME/111autodlrutorrentfix.sh $HOME/222autodlrutorrentfix.sh
-chmod -f 700 $HOME/bin/autodlrutorrentfix
+cd && rm -f {000,111,222}autodlrutorrentfix.sh
+chmod -f 700 "$HOME/bin/autodlrutorrentfix"
 #
 ############################
 ##### Self Updater End #####
@@ -85,14 +78,14 @@ chmod -f 700 $HOME/bin/autodlrutorrentfix
 #
 read -ep "The script has been updated, do you wish to continue [y] or exit now [q] : " updatestatus
 echo
-if [[ $updatestatus =~ ^[Yy]$ ]]
+if [[ "$updatestatus" =~ ^[Yy]$ ]]
 then
 #
 ############################
 ####### Script Start #######
 ############################
     #
-    if [ -d ~/.irssi/scripts/AutodlIrssi/ ]
+    if [[ -d ~/.irssi/scripts/AutodlIrssi ]]
     then
         echo -e "\033[33m""Autodl Before""\e[0m"
         echo -e "\033[32m""~/.irssi/scripts/AutodlIrssi/GuiServer.pm =" "\033[31m""$(sed -n "s/use constant LISTEN_ADDRESS => '\(.*\)';/\1/p" ~/.irssi/scripts/AutodlIrssi/GuiServer.pm 2> /dev/null)""\e[0m"
@@ -118,7 +111,7 @@ then
         exit
     fi
     #
-    if [ -d ~/www/$(whoami).$(hostname)/public_html/rutorrent/plugins/autodl-irssi/ ]
+    if [[ -d ~/www/$(whoami).$(hostname)/public_html/rutorrent/plugins/autodl-irssi ]]
     then
         echo -e "\033[33m""Autodl-rutorrent Before""\e[0m"
         echo -e "\033[32m""/rutorrent/plugins/autodl-irssi/getConf.php =" "\033[31m""$(sed -n 's/\(.*\)if (\!socket_connect($socket, "\(.*\)", $autodlPort))/\2/p' ~/www/$(whoami).$(hostname)/public_html/rutorrent/plugins/autodl-irssi/getConf.php 2> /dev/null)""\e[0m"
@@ -154,6 +147,6 @@ then
 #
 else
     echo -e "You chose to exit after updating the scripts."
-    exit 1
     cd && bash
+    exit 1
 fi
