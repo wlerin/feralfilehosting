f504f1bf176a88ee89a70e9895f333dfa0e6b97e
randomessence <random@digikool.co.uk>
2013-12-28 10:46:03 +0000

    autodl port

--- Feral Wiki/Software/Autodl-irssi and rutorrent plugin - community edition/scripts/autodlport.sh
+++ Feral Wiki/Software/Autodl-irssi and rutorrent plugin - community edition/scripts/autodlport.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # autodlport.sh
-scriptversion="1.0.2"
+scriptversion="1.0.3"
 scriptname="autodlport"
 # randomessence
 #
@@ -10,6 +10,7 @@ scriptname="autodlport"
 ## Version History Starts ##
 ############################
 #
+# 1.0.3 general formatting
 # 1.0.2 generates optional password for the user like the install script.
 #
 ############################
@@ -20,6 +21,7 @@ scriptname="autodlport"
 ###### Variable Start ######
 ############################
 #
+port=$(shuf -i 6000-50000 -n 1)
 pass=$(< /dev/urandom tr -dc '12345!@#ANCDEFGHIJKLMNOPabcdefghijklmnop' | head -c${1:-20};echo;)
 scripturl="https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Autodl-irssi%20and%20rutorrent%20plugin%20-%20community%20edition/scripts/autodlport.sh"
 #
@@ -85,10 +87,11 @@ then
 ####### Script Start #######
 ############################
 #
-    port=$(shuf -i 6000-50000 -n 1)
-    if [[ -f ~/.autodl/autodl.cfg && -f ~/www/$(whoami).$(hostname)/public_html/rutorrent/plugins/autodl-irssi/conf.php ]]
+    if [[ -f "$HOME/.autodl/autodl.cfg" && -f "$HOME/www/$(whoami).$(hostname)/public_html/rutorrent/plugins/autodl-irssi/conf.php" ]]
     then
         echo "This script will change your password and port for autodl and the rutorrent plugin, then restart irssi."
+        echo
+        echo -e "\033[31m""A randomly generated password has been set for you, you can just press enter""\e[0m"
         echo
         read -ep "Enter Password (No spaces please!): " -i "$pass" pass
         echo
@@ -96,11 +99,11 @@ then
         echo
         read -ep "Please confirm this is the password you wish to use [y/n]: " confirm
         echo
-        if [[ $confirm =~ ^[Yy]$ ]]
+        if [[ "$confirm" =~ ^[Yy]$ ]]
         then
-            sed -ri 's/(.*)gui-server-port =(.*)/gui-server-port = '$port'/g' ~/.autodl/autodl.cfg
-            sed -ri 's/(.*)gui-server-password =(.*)/gui-server-password = '$pass'/g' ~/.autodl/autodl.cfg
-            echo -e "<?php\n\$autodlPort = $port;\n\$autodlPassword = \"$pass\";\n?>" > ~/www/$(whoami).$(hostname)/public_html/rutorrent/plugins/autodl-irssi/conf.php
+            sed -ri 's/(.*)gui-server-port =(.*)/gui-server-port = '"$port"'/g' "$HOME/.autodl/autodl.cfg"
+            sed -ri 's/(.*)gui-server-password =(.*)/gui-server-password = '"$pass"'/g' "$HOME/.autodl/autodl.cfg"
+            echo -e "<?php\n\$autodlPort = $port;\n\$autodlPassword = \"$pass\";\n?>" > "$HOME/www/$(whoami).$(hostname)/public_html/rutorrent/plugins/autodl-irssi/conf.php"
             killall -9 irssi -u $(whoami) 2> /dev/null 
             screen -wipe > /dev/null 2>&1
             screen -dmS autodl irssi
@@ -109,17 +112,17 @@ then
             echo -e "\033[32m""screen -r autodl""\e[0m"
             echo
         else
-            bash ~/autodlport.sh
+            bash "$HOME/autodlport.sh"
         fi        
     else
         echo
         echo -e "The required files do not exist, please install autodl irssi using the bash script first."
         echo
-        if [[ ! -f ~/.autodl/autodl.cfg ]]
+        if [[ ! -f "$HOME/.autodl/autodl.cfg" ]]
         then
             echo -e "\033[36m""~/.autodl/autodl.cfg""\e[0m"" is missing"
         fi
-        if [[ ! -f ~/www/$(whoami).$(hostname)/public_html/rutorrent/plugins/autodl-irssi/conf.php ]]
+        if [[ ! -f "$HOME/www/$(whoami).$(hostname)/public_html/rutorrent/plugins/autodl-irssi/conf.php" ]]
         then
             echo -e "\033[36m""/rutorrent/plugins/autodl-irssi/conf.php""\e[0m"" is missing"
         fi
