922849d675d57d85418593f9acf9f0fedd8ca9fe
feralhosting <feralhosting@digikool.co.uk>
2014-03-28 14:11:24 +0000

    scripts

--- Feral Wiki/HTTP/Redirecting HTTP to HTTPS/scripts/nginxhttps.sh
+++ Feral Wiki/HTTP/Redirecting HTTP to HTTPS/scripts/nginxhttps.sh
@@ -1,128 +1,152 @@
 #!/bin/bash
-# Script name
-scriptversion="1.0.1"
+# nginx force https on Dual URL
+scriptversion="1.0.2"
 scriptname="nginxhttps"
-# Author name
+#
+# wget -qO ~/nginxhttps.sh http://git.io/A34SpA && bash ~/nginxhttps.sh
 #
 ############################
 ## Version History Starts ##
 ############################
 #
-# wget -qNO ~/nginxhttps.sh http://git.io/A34SpA && bash ~/nginxhttps.sh
+# v1.0.2 template updated
 #
 ############################
 ### Version History Ends ###
 ############################
 #
-#
-#
 ############################
 ###### Variable Start ######
 ############################
 #
-#
+scripturl="https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/HTTP/Redirecting%20HTTP%20to%20HTTPS/scripts/nginxhttps.sh"
 #
 ############################
 ####### Variable End #######
 ############################
 #
-#
-#
 ############################
 #### Self Updater Start ####
 ############################
 #
-if [ ! -f $HOME/nginxhttps.sh ]
+mkdir -p "$HOME/bin"
+#
+if [[ ! -f "$HOME/$scriptname.sh" ]]
 then
-    wget -qNO $HOME/nginxhttps.sh https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/HTTP/Redirecting%20HTTP%20to%20HTTPS/scripts/nginxhttps.sh
+    wget -qO "$HOME/$scriptname.sh" "$scripturl"
+fi
+if [[ ! -f "$HOME/bin/$scriptname" ]]
+then
+    wget -qO "$HOME/bin/$scriptname" "$scripturl"
 fi
 #
-wget -qNO $HOME/000nginxhttps.sh https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/HTTP/Redirecting%20HTTP%20to%20HTTPS/scripts/nginxhttps.sh
+wget -qO "$HOME/000$scriptname.sh" "$scripturl"
 #
-if ! diff -q $HOME/000nginxhttps.sh $HOME/nginxhttps.sh > /dev/null 2>&1
+if ! diff -q "$HOME/000$scriptname.sh" "$HOME/$scriptname.sh" > /dev/null 2>&1
 then
     echo '#!/bin/bash
-    wget -qNO $HOME/nginxhttps.sh https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/HTTP/Redirecting%20HTTP%20to%20HTTPS/scripts/nginxhttps.sh
-    bash $HOME/nginxhttps.sh
-    exit 1' > $HOME/111nginxhttps.sh
-    bash $HOME/111nginxhttps.sh
+    scriptname="'"$scriptname"'"
+    wget -qO "$HOME/$scriptname.sh" "'"$scripturl"'"
+    wget -qO "$HOME/bin/$scriptname" "'"$scripturl"'"
+    bash "$HOME/$scriptname.sh"
+    exit 1' > "$HOME/111$scriptname.sh"
+    bash "$HOME/111$scriptname.sh"
     exit 1
 fi
-#
-echo
-echo -e "Hello $(whoami), you have the latest version of the" "\033[36m""$scriptname""\e[0m" "script. This script version is:" "\033[31m""$scriptversion""\e[0m"
-echo
-#
-rm -f $HOME/000nginxhttps.sh $HOME/111nginxhttps.sh
+if ! diff -q "$HOME/000$scriptname.sh" "$HOME/bin/$scriptname" > /dev/null 2>&1
+then
+    echo '#!/bin/bash
+    scriptname="'"$scriptname"'"
+    wget -qO "$HOME/$scriptname.sh" "'"$scripturl"'"
+    wget -qO "$HOME/bin/$scriptname" "'"$scripturl"'"
+    bash "$HOME/$scriptname.sh"
+    exit 1' > "$HOME/222$scriptname.sh"
+    bash "$HOME/222$scriptname.sh"
+    exit 1
+fi
+cd && rm -f {000,111,222}"$scriptname.sh"
+chmod -f 700 "$HOME/bin/$scriptname"
 #
 ############################
 ##### Self Updater End #####
 ############################
 #
+############################
+#### Core Script Starts ####
+############################
+#
+echo
+echo -e "Hello $(whoami), you have the latest version of the" "\033[36m""$scriptname""\e[0m" "script. This script version is:" "\033[31m""$scriptversion""\e[0m"
+echo
 read -ep "The scripts have been updated, do you wish to continue [y] or exit now [q] : " updatestatus
 echo
-if [[ $updatestatus =~ ^[Yy]$ ]]
+if [[ "$updatestatus" =~ ^[Yy]$ ]]
 then
 #
 ############################
-####### Script Start #######
+#### User Script Starts ####
 ############################
 #
-echo "1: Creating the conf file"
-echo
-#
-echo -n "# Set the variable so we can also redirect to server.feralhosting.com/username/
-set \$feraluser  \"$(whoami)\";
+    echo "1: Creating the conf file"
+    echo
+    #
+    echo -n "# Set the variable so we can also redirect to server.feralhosting.com/username/
+    set \$feraluser  \"$(whoami)\";
 
-# Checks if https is NOT on and then SETs part one of our custom variable.
-if (\$http_x_forwarded_proto != https ) {
-    set \$forceh  A;
-}
+    # Checks if https is NOT on and then SETs part one of our custom variable.
+    if (\$http_x_forwarded_proto != https ) {
+        set \$forceh  A;
+    }
 
-# Check for the URL format username.server.feralhosting.com and SETs part 2 of our custom variable.
-if (\$http_x_host = \$http_host) {
-    set \$forceh  \"\${forceh}1\"; 
-}
+    # Check for the URL format username.server.feralhosting.com and SETs part 2 of our custom variable.
+    if (\$http_x_host = \$http_host) {
+        set \$forceh  \"\${forceh}1\"; 
+    }
 
-# Check for the URL format server.feralhosting.com/username and SETs part 2 of our custom variable.
-if (\$http_x_host != \$http_host) {
-    set \$forceh  \"\${forceh}2\";
-}
+    # Check for the URL format server.feralhosting.com/username and SETs part 2 of our custom variable.
+    if (\$http_x_host != \$http_host) {
+        set \$forceh  \"\${forceh}2\";
+    }
 
-# Check x_host when it does match host that it is also specific to feral, so as not to apply to a users's VHost domains.
-if (\$http_x_host ~* ^([0-9a-zA-Z-]+)\.([0-9a-zA-Z-]+)\.feralhosting\.com\$) {
-    set \$forceh  \"\${forceh}3\";
-}
+    # Check x_host when it does match host that it is also specific to feral, so as not to apply to a users's VHost domains.
+    if (\$http_x_host ~* ^([0-9a-zA-Z-]+)\.([0-9a-zA-Z-]+)\.feralhosting\.com\$) {
+        set \$forceh  \"\${forceh}3\";
+    }
 
-# Check x_host when it does not match host that it is also specific to feral, so as not to apply to a users's VHost domains.
-if (\$http_x_host ~* ^([0-9a-zA-Z-]+)\.feralhosting\.com\$) {
-    set \$forceh  \"\${forceh}4\";
-}
+    # Check x_host when it does not match host that it is also specific to feral, so as not to apply to a users's VHost domains.
+    if (\$http_x_host ~* ^([0-9a-zA-Z-]+)\.feralhosting\.com\$) {
+        set \$forceh  \"\${forceh}4\";
+    }
 
-# Combines the SET options to rewrite this URL format: username.server.feralhosting.com
-if (\$forceh = A13) {
-    rewrite ^ https://\$http_x_host\$request_uri? permanent;
-}
+    # Combines the SET options to rewrite this URL format: username.server.feralhosting.com
+    if (\$forceh = A13) {
+        rewrite ^ https://\$http_x_host\$request_uri? permanent;
+    }
 
-# Combines the SET options to rewrite this URL format: server.feralhosting.com/username
-if (\$forceh = A24) {
-    rewrite ^ https://\$http_x_host/\$feraluser\$request_uri? permanent;
-}" > ~/.nginx/conf.d/000-default-server.d/https.conf
-#
-echo "2: Reloading the nginx configuration"
-echo
-# killall -9 nginx php5-fpm -u $(whoami) 2> /dev/null
-/usr/sbin/nginx -s reload -c ~/.nginx/nginx.conf > /dev/null 2>&1
-# echo "Please wait up to 5 minutes for nginx to restart."
-echo "3: Done. You may need to clear your browser cache to see the changes."
-echo
+    # Combines the SET options to rewrite this URL format: server.feralhosting.com/username
+    if (\$forceh = A24) {
+        rewrite ^ https://\$http_x_host/\$feraluser\$request_uri? permanent;
+    }" > ~/.nginx/conf.d/000-default-server.d/https.conf
+    #
+    echo "2: Reloading the nginx configuration"
+    echo
+    /usr/sbin/nginx -s reload -c ~/.nginx/nginx.conf > /dev/null 2>&1
+    echo "3: Done. You may need to clear your browser cache to see the changes."
+    echo
+    exit 1
 #
 ############################
-####### Script Ends  #######
+##### User Script End  #####
 ############################
 #
 else
     echo -e "You chose to exit after updating the scripts."
-    exit 1
+    echo
     cd && bash
+    exit 1
 fi
+#
+############################
+##### Core Script Ends #####
+############################
+#
