58f1bcdf9bdbadf966dbb167a32ea884b2e249bb
feralhosting <feralhosting@digikool.co.uk>
2014-03-28 14:07:36 +0000

    scripts

--- Feral Wiki/HTTP/Updating Apache to nginx/scripts/rutorrentnginx.sh
+++ Feral Wiki/HTTP/Updating Apache to nginx/scripts/rutorrentnginx.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# randomessence
 # wget -qO ~/rutnginx.sh http://git.io/9vlcyw && bash ~/rutnginx.sh
 echo 'location /rutorrent {
     auth_basic '$(whoami)';
-    auth_basic_user_file '$HOME/www/$(whoami).$(hostname)/public_html/rutorrent/.htpasswd';
+    auth_basic_user_file '$HOME'/www/'$(whoami)'.'$(hostname)'/public_html/rutorrent/.htpasswd;
 }
 
 location /rutorrent/conf { deny all; }
 location /rutorrent/share { deny all; }' > ~/.nginx/conf.d/000-default-server.d/rutorrent.conf
-# killall -9 nginx php5-fpm -u $(whoami)
 /usr/sbin/nginx -s reload -c ~/.nginx/nginx.conf > /dev/null 2>&1
+echo "Done, reload rutorrent in your browser to see if it worked."
+echo
