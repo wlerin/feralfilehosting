64f5f9062fc501f07fd711b5bb8a86324b720eb5
feralhosting <feralhosting@digikool.co.uk>
2014-10-08 14:15:13 +0100

    tweaks

--- Feral Wiki/0 Templates/menu.sh
+++ Feral Wiki/0 Templates/menu.sh
@@ -7,20 +7,29 @@ showMenu ()
         echo "1) bob"
         echo "2) amy"
         echo "3) quit"
+        echo
 }
 
 while [ 1 ]
 do
         showMenu
-        read CHOICE
+        read -e CHOICE
+        echo
         case "$CHOICE" in
                 "1")
+                        echo
                         echo "Bob was here"
+                        echo
                         ;;
                 "2")
+                        echo
                         echo "Amy was here"
+                        echo
                         ;;
                 "3")
+                        echo
+                        echo "You chose to quit the script."
+                        echo
                         exit
                         ;;
         esac
