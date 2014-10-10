3c57ee559e2db75283324d32cfc3028cee8e4c41
feralhosting <feralhosting@digikool.co.uk>
2014-10-10 13:28:42 +0100

    menu

--- Feral Wiki/0 Templates/menu.sh
+++ Feral Wiki/0 Templates/menu.sh
@@ -1,36 +1,131 @@
 #!/bin/bash
+# Script name
+scriptversion="0.0.0"
+scriptname="somescript"
+# Author name
+#
+# Bash Command
+#
+############################
+## Version History Starts ##
+############################
+#
+# How do I customise this updater? 
+# 1: scriptversion="0.0.0" replace "0.0.0" with your script version. This will be shown to the user at the current version.
+# 2: scriptname="somescript" replace "somescript" with your script name. Make it unique to this script.
+# 3: Set the scripturl variable in the variable section to the RAW github URl of the script for updating.
+# 4: Insert your script in the "Script goes here" labelled section
+#
+# This updater deals with updating a single file, the "~/bin/somescript", by updating and switching to this script.
 #
 # http://grover.open2space.com/content/bash-script-menus-and-functions
+#
+############################
+### Version History Ends ###
+############################
+option1="Install Program"
+option2="Update Program"
+option3="Quit the Script"
+############################
+###### Variable Start ######
+############################
+#
+scripturl="https://raw.github.com/feralhosting"
+#
+############################
+####### Variable End #######
+############################
+#
+############################
+#### Self Updater Start ####
+############################
+#
+[[ ! -d ~/bin ]] && mkdir -p ~/bin
+[[ ! -f ~/bin/"$scriptname" ]] && wget -qO ~/bin/"$scriptname" "$scripturl"
+#
+wget -qO ~/.000"$scriptname" "$scripturl"
+#
+if [[ $(sha256sum ~/.000"$scriptname" | awk '{print $1}') != $(sha256sum ~/bin/"$scriptname" | awk '{print $1}') ]]
+then
+    echo -e "#!/bin/bash\nwget -qO ~/bin/$scriptname $scripturl\ncd && rm -f $scriptname{.sh,}\nbash ~/bin/$scriptname\nexit" > ~/.111"$scriptname"
+    bash ~/.111"$scriptname"
+    exit
+else
+    if [[ -z $(ps x | fgrep "bash $HOME/bin/$scriptname" | grep -v grep | head -n 1 | awk '{print $1}') && $(ps x | fgrep "bash $HOME/bin/$scriptname" | grep -v grep | head -n 1 | awk '{print $1}') -ne "$$" ]]
+    then
+        echo -e "#!/bin/bash\ncd && rm -f $scriptname{.sh,}\nbash ~/bin/$scriptname\nexit" > ~/.222"$scriptname"
+        bash ~/.222"$scriptname"
+        exit
+    fi
+fi
+cd && rm -f .{000,111,222}"$scriptname"
+chmod -f 700 ~/bin/"$scriptname"
+#
+############################
+##### Self Updater End #####
+############################
+#
+############################
+#### Core Script Starts ####
+############################
+#
+echo
+echo -e "Hello $(whoami), you have the latest version of the" "\033[36m""$scriptname""\e[0m" "script. This script version is:" "\033[31m""$scriptversion""\e[0m"
+echo
+read -ep "The scripts have been updated, do you wish to continue [y] or exit now [q] : " -i "y" updatestatus
+echo
+if [[ "$updatestatus" =~ ^[Yy]$ ]]
+then
+#
+############################
+#### User Script Starts ####
+############################
+#
+    showMenu () 
+    {
+            echo "1) $option1"
+            echo "2) $option2"
+            echo "3) $option3"
+            echo
+    }
 
-showMenu () 
-{
-        echo "1) bob"
-        echo "2) amy"
-        echo "3) quit"
-        echo
-}
-
-while [ 1 ]
-do
-        showMenu
-        read -e CHOICE
-        echo
-        case "$CHOICE" in
-                "1")
-                        echo
-                        echo "Bob was here"
-                        echo
-                        ;;
-                "2")
-                        echo
-                        echo "Amy was here"
-                        echo
-                        ;;
-                "3")
-                        echo
-                        echo "You chose to quit the script."
-                        echo
-                        exit
-                        ;;
-        esac
-done
+    while [ 1 ]
+    do
+            showMenu
+            read -e CHOICE
+            echo
+            case "$CHOICE" in
+                    "1")
+                            echo
+                            echo "Bob was here"
+                            echo
+                            ;;
+                    "2")
+                            echo
+                            echo "Amy was here"
+                            echo
+                            ;;
+                    "3")
+                            echo
+                            echo "You chose to quit the script."
+                            echo
+                            exit
+                            ;;
+            esac
+    done
+#
+############################
+##### User Script End  #####
+############################
+#
+else
+    echo -e "You chose to exit after updating the scripts."
+    echo
+    cd && bash
+    exit 1
+fi
+#
+############################
+##### Core Script Ends #####
+############################
+#
