673c9aaeff5fb13bc5086c4cf2976479d689ed47
feralhosting <feralhosting@digikool.co.uk>
2014-10-29 09:13:50 +0000

    dualurl

--- Feral Wiki/0 Templates/dualurl-2.php
+++ Feral Wiki/0 Templates/dualurl-2.php
@@ -1,4 +1,5 @@
 <?php
+# http://git.io/GlFpRQ
 if ($_SERVER['HTTP_X_HOST'] != $_SERVER['HTTP_HOST']){  // HTTP_HOST is always the same format username.server.feralhosting.com
 $GLOBALS['xproxy'] = '/' . getenv('USER');              // If the HOSTs don't match then insert this variable
 } else {                                                // If they do match then do this instead
