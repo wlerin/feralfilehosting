1a0a316d0f7bbd1eebc670632f4c41bb9b985e92
feralhosting <feralhosting@digikool.co.uk>
2014-10-31 19:39:03 +0000

    wfg

---
+++ Feral Wiki/Software/whatfreegrab/scripts/WFG-setup.py
@@ -0,0 +1,266 @@
+#!/usr/bin/env python2
+
+import getpass
+import os
+import random
+import shutil
+import sys
+import urllib2
+import zipfile
+
+import cStringIO as StringIO
+import ConfigParser
+
+SCRIPT_DIR  = os.path.dirname(os.path.realpath(sys.argv[0]))
+CONFIG_FILE = os.path.join(SCRIPT_DIR, 'wfg.cfg')
+
+
+def setup():
+    print MESSAGES['welcome']
+
+    pause()
+
+    print "First, let's setup the WhatFreeGrab dependencies."
+    print ""
+
+    try:
+        print "Installing/updating Requests module: ",
+        download_module("requests", "https://github.com/kennethreitz/requests/archive/v2.0.1.zip")
+        import requests
+    except:
+        print MESSAGES['module_error'] % "requests"
+        raise
+
+    try:
+        print "Installing/updating WhatAPI module: ",
+        download_module('whatapi', 'https://github.com/emjaytee404/whatapi/archive/stable.zip')
+        from whatapi import whatapi
+    except:
+        print MESSAGES['module_error'] % "whatapi"
+        raise
+
+    pause()
+
+    if not sys.getfilesystemencoding().upper() in ('UTF-8', 'MBCS'):
+        print MESSAGES['filesystem_error']
+        sys.exit(1)
+
+    config = ConfigParser.RawConfigParser()
+    config.read(CONFIG_FILE)
+
+    while True:
+
+        print "Next we will need your What.CD username and password."
+
+        prompt = "Enter your username"
+        try:
+            username = config.get('login', 'username')
+            prompt += " [" + username + "]: "
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+            username = None
+            prompt += ": "
+
+        username = raw_input(prompt) or username
+        if not username:
+            continue
+
+        prompt = "Enter your password (will not be shown)"
+        try:
+            password = config.get('login', 'password')
+            prompt += " [<saved password>]: "
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+            password = None
+            prompt += ": "
+
+        password = getpass.getpass(prompt) or password
+        if not password:
+            continue
+
+        print ""
+        print "Attempting login...",
+
+        try:
+            whatapi.WhatAPI(username=username, password=password).logout()
+        except whatapi.LoginException:
+            print "failed. :("
+            print "Let's try again."
+            continue
+        except:
+            raise
+        else:
+            print "success!"
+            break
+
+    try:
+        config.add_section('login')
+    except ConfigParser.DuplicateSectionError:
+        pass
+
+    config.set('login', 'username', username)
+    config.set('login', 'password', password)
+
+    pause()
+
+    while True:
+
+        print "The directory where the script downloads torrent files is called the target."
+
+        prompt = "Enter target"
+        try:
+            target = config.get('download', 'target')
+            prompt += " [" + target + "]: "
+        except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
+            target = None
+            prompt += ": "
+
+        target = raw_input(prompt) or target
+
+        full_target = os.path.realpath(os.path.expanduser(target))
+
+        if not os.path.exists(full_target):
+            try:
+                os.makedirs(full_target)
+            except:
+                print "Unable to access the '%s' directory." % target
+                print "Let's try again."
+                continue
+            else:
+                print ""
+                print "Looks good."
+                break
+        else:
+            print ""
+            print "Looks good."
+            break
+
+    try:
+        config.add_section('download')
+    except ConfigParser.DuplicateSectionError:
+        pass
+
+    config.set('download', 'target', target)
+
+    pause()
+
+    with open(CONFIG_FILE, 'w') as f:
+        config.write(f)
+
+    print MESSAGES['config-finished'] % CONFIG_FILE
+
+    pause()
+
+    script_path = os.path.join(SCRIPT_DIR, "WFG.py")
+
+    print MESSAGES['skip-downloads'] % script_path
+
+    pause()
+
+    rand_minutes = str(random.randrange(60)).zfill(2)
+
+    print MESSAGES['cron'] % (rand_minutes, script_path)
+
+    pause()
+
+    print MESSAGES['finished']
+
+    pause("Press ENTER to exit. ")
+
+def pause(msg="Press ENTER to continue... "):
+    print ""
+    raw_input(msg)
+    print "-" * 80
+    print ""
+
+def download_module(module_name, module_url):
+
+    print "downloading... ",
+
+    data = urllib2.urlopen(module_url)
+    data = StringIO.StringIO(data.read())
+    data = zipfile.ZipFile(data)
+
+    filelist = data.namelist()
+    root     = filelist[0]
+    dirname  = os.path.join(root, module_name)
+    filelist = [filename for filename in filelist if filename.startswith(dirname)]
+
+    cwd = os.getcwd()
+
+    os.chdir(SCRIPT_DIR)
+
+    print "extracting... ",
+
+    data.extractall(members=filelist)
+
+    # Remove previous version first.
+    shutil.rmtree(module_name, ignore_errors=True)
+
+    os.rename(dirname, module_name)
+    os.rmdir(root)
+
+    os.chdir(cwd)
+
+    print "done!"
+
+
+MESSAGES = {}
+
+MESSAGES['welcome'] = """
+WhatFreeGrab Setup
+------------------
+
+Hey there! This little program will help you setup WhatFreeGrab.
+
+You will need your What.CD username and password, so have those ready.
+"""
+MESSAGES['module_error'] = """
+An error ocurred trying to download/extract the '%s' module.
+Please try to run this script again.
+"""
+MESSAGES['filesystem_error'] = """
+Your filesystem encoding is not able to handle Unicode filenames. This means
+that files containing non-Latin characters in their names will not be able to
+be saved. Please read your system's documentation for changing the encoding
+used to save filenames.
+
+On Linux, this is accomplished by changing your locale to one supporting UTF-8.
+"""
+MESSAGES['config-finished'] = """
+Configuration file created successfully.
+
+You can re-run this setup file at any time to change the settings, or you can
+change the values directly. The config file is located at:
+
+%s
+"""
+MESSAGES['skip-downloads'] = """
+If you want to run the script and have it record all current freeleech torrents
+without downloading any of them you can do so like this:
+
+python2 %s --skip-downloads
+"""
+MESSAGES['cron'] = """
+If you plan on adding the script to your cron file, consider using the
+following line:
+
+%s * * * * python2 %s
+
+The minutes field above has been selected at random.
+
+Spreading the scheduling like this helps avoid having a bunch of scripts all
+hitting the server every hour on the hour.
+"""
+MESSAGES['finished'] = """
+The setup is now complete.
+
+Enjoy!
+"""
+
+if __name__ == '__main__':
+    try:
+        setup()
+    except KeyboardInterrupt:
+        print "Setup process had been cancelled."
+    except:
+        print "Unhandled error has occurred:"
+        raise
--- Feral Wiki/Software/whatfreegrab/scripts/WFG.py
+++ Feral Wiki/Software/whatfreegrab/scripts/WFG.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 import htmlentitydefs
-import logging
-import logging.handlers
 import os
 import re
 import string
@@ -13,112 +11,46 @@ import time
 import ConfigParser
 import cPickle as pickle
 
-SCRIPT_DIR  = os.path.dirname(os.path.realpath(sys.argv[0]))
-CONFIG_FILE = os.path.join(SCRIPT_DIR, 'wfg.cfg')
-STATE_FILE  = os.path.join(SCRIPT_DIR, 'wfg.dat')
-LOCK_FILE   = os.path.join(SCRIPT_DIR, 'wfg.pid')
-LOG_FILE    = os.path.join(SCRIPT_DIR, 'wfg.log')
-
-try:
-    import requests
-except ImportError:
-    def download_requests():
-
-        print "It seems you are missing the required 'requests' module."
-        print "Press ENTER to automatically download and extract this module."
-        print "Press Ctrl+C if you wish to cancel and install it manually."
-        try:
-            raw_input()
-        except KeyboardInterrupt:
-            print "You must install the 'requests' module before attempting to run this script."
-            print "Visit http://docs.python-requests.org/en/latest/user/install/ for instructions."
-            sys.exit(1)
-
-        import urllib2
-        import zipfile
-
-        import cStringIO as StringIO
-
-        requests_zip = "https://github.com/kennethreitz/requests/archive/v2.0.1.zip"
-
-        data = urllib2.urlopen(requests_zip)
-        data = StringIO.StringIO(data.read())
-        data = zipfile.ZipFile(data)
-
-        filelist = data.namelist()
-        root = filelist[0]
-        dirname = os.path.join(root, 'requests')
-        filelist = [filename for filename in filelist if filename.startswith(dirname)]
-
-        cwd = os.getcwd()
-
-        os.chdir(SCRIPT_DIR)
-
-        data.extractall(members=filelist)
-
-        os.rename(dirname, 'requests')
-        os.rmdir(root)
-
-        os.chdir(cwd)
-
-        print "Extraction complete. Will attempt to continue..."
-
-    download_requests()
-    import requests
+import requests
+import whatapi
 
 class WFGException(Exception): pass
 
 class WhatFreeGrab(object):
 
-    NAME  = "WhatFreeGrab"
-    VER   = "0.1"
-    IDENT = "%s v%s" % (NAME, VER)
+    IDENT = "WhatFreeGrab v0.1"
 
     INVALID_CHARS = r'\/:*<>|?"'
     HTML_RE = re.compile("&#?\w+;")
 
-    headers = {
-        'Content-type': "application/x-www-form-urlencoded",
-        'Accept-Charset': "utf-8",
-        'User-Agent': IDENT
-    }
-
-    loginpage   = "https://what.cd/login.php"
-    ajaxpage    = "https://what.cd/ajax.php"
-    torrentpage = "https://what.cd/torrents.php"
+    SCRIPT_DIR  = os.path.dirname(os.path.realpath(sys.argv[0]))
+    CONFIG_FILE = os.path.join(SCRIPT_DIR, 'wfg.cfg')
+    STATE_FILE  = os.path.join(SCRIPT_DIR, 'wfg.dat')
+    LOCK_FILE   = os.path.join(SCRIPT_DIR, 'wfg.pid')
 
     defaults = {
-        'log_level': "INFO",
         'max_torrents': "3000",
         'quiet': "false",
         'template_music': "${artist} - ${groupName} (${format} ${encoding}) [${torrentId}]",
         'template_other': "${groupName} [${torrentId}]"
     }
 
-    timeformat = "%Y-%m-%d %H:%M:%S"
+    def __init__(self, config_file=None, state_file=None, lock_file=None):
 
-    log_size = 10 * 1024 * 1024 # 10MB
-
-    def __init__(self, config_file, state_file, lock_file, log_file):
-
-        self.config_file = config_file
-        self.state_file  = state_file
-        self.lock_file   = lock_file
-        self.log_file    = log_file
+        self.config_file = config_file or WhatFreeGrab.CONFIG_FILE
+        self.state_file  = state_file or WhatFreeGrab.STATE_FILE
+        self.lock_file   = lock_file or WhatFreeGrab.LOCK_FILE
 
         self.instance = SingleInstance(self.lock_file)
 
         self.start_time = time.time()
 
-        self.session = requests.session()
-        self.session.headers = WhatFreeGrab.headers
+        self.config = ConfigParser.RawConfigParser(WhatFreeGrab.defaults)
 
-        self.config = ConfigParser.SafeConfigParser(WhatFreeGrab.defaults)
-
-        if not os.path.exists(self.config_file):
-            self._first_run()
-
-        self.config.read(self.config_file)
+        try:
+            self.config.read(self.config_file)
+        except:
+            self.quit("Unable to read configuration file.", error=True)
 
         # This is necessary because otherwise we get 'NoSectionError' even if
         # the value is set in the defaults.
@@ -128,24 +60,10 @@ class WhatFreeGrab(object):
             pass
 
         self.quiet = self.config.getboolean('output', 'quiet')
-        self.log_level = self.config.get('output', 'log_level')
-
-        numeric_level = getattr(logging, self.log_level.upper(), None)
-        if not isinstance(numeric_level, int):
-            raise ValueError('Invalid log level: %s' % self.log_level)
-
-        self.log = logging.getLogger()
-        self.log.setLevel(logging.INFO)
-        handler = logging.handlers.RotatingFileHandler(filename=log_file, maxBytes=WhatFreeGrab.log_size, encoding="utf-8")
-        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", datefmt=WhatFreeGrab.timeformat)
-        handler.setFormatter(formatter)
-        self.log.addHandler(handler)
-
-        self.log.info("%s starting up", WhatFreeGrab.IDENT)
 
         self.message(WhatFreeGrab.IDENT)
         self.message("-" * len(WhatFreeGrab.IDENT))
-        self.message("Startup time: %s" % time.strftime(WhatFreeGrab.timeformat, time.localtime(self.start_time)))
+        self.message("Startup time: %s" % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(self.start_time)))
 
         self.username = self.config.get('login', 'username')
         self.password = self.config.get('login', 'password')
@@ -164,6 +82,8 @@ class WhatFreeGrab(object):
             os.makedirs(self.target)
 
         self.max_torrents = self.config.getint('download', 'max_torrents')
+
+        self.skip_downloads = '--skip-downloads' in sys.argv
 
         self.template_music = self.config.get('download', 'template_music')
         self.template_other = self.config.get('download', 'template_other')
@@ -184,168 +104,30 @@ class WhatFreeGrab(object):
         if not self.filters:
             self.filters = [{}]
 
-        if os.path.exists(self.state_file):
+        try:
             self.state = pickle.load(open(self.state_file, 'rb'))
-        else:
+        except:
             self.state = {}
 
-        if 'cookies' in self.state:
-            self.session.cookies = self.state['cookies']
-        else:
-            self._login()
-
-        self.authkey = None
-        self.passkey = None
-
+        cookies = self.state.get('cookies')
         try:
-            self._get_accountinfo()
-        except WFGException: # Expired/invalid cookie?
-            try:
-                self._login()
-            except WFGException:
-                self.quit("Unable to login. Check your configuration.", error=True)
-            else:
-                self._get_accountinfo()
+            self.what = whatapi.WhatAPI(config_file=self.config_file, cookies=cookies)
+        except whatapi.whatapi.LoginException:
+            self.quit("Unable to login. Check your configuration.", error=True)
+        self.state['cookies'] = self.what.session.cookies
+        self.save_state()
 
-        self.history = self.state.get('history', set())
+        if not 'history' in self.state:
+            self.state['history'] = set()
+
         self.torrent_list = []
 
         self.counter = {}
         for key in 'total', 'downloaded', 'skipped', 'exists', 'error':
             self.counter[key] = 0
 
-    def _first_run(self):
-        import getpass
-        import random
-
-        config = ConfigParser.SafeConfigParser()
-        rand_minutes = str(random.randrange(60)).zfill(2)
-        script_path = os.path.join(SCRIPT_DIR, sys.argv[0])
-
-        message = """
-Hey there! It looks like you are running this script for the first time.
-
-Let's go ahead and create a new configuration file."""
-
-        print message
-
-        while True:
-
-            print "\nFirst we will need your What.CD username and password."
-
-            username = raw_input("Enter your username: ")
-            if not username:
-                continue
-
-            password = getpass.getpass("Enter your password (will not be shown on screen): ")
-            if not password:
-                continue
-
-            print "\nGot it. The script will try to login with this info...",
-
-            self.username = username
-            self.password = password
-
-            try:
-                self._login()
-            except WFGException:
-                print "failed. :("
-                print "Let's try again."
-                continue
-            else:
-                print "success!"
-                break
-
-        config.add_section('login')
-        config.set('login', 'username', username)
-        config.set('login', 'password', password)
-
-        while True:
-
-            print "\nThe directory where the script downloads .torrent files is called the target."
-
-            target = raw_input("Enter target: ")
-
-            full_target = os.path.realpath(os.path.expanduser(target))
-
-            if not os.path.exists(full_target):
-                try:
-                    os.makedirs(full_target)
-                except:
-                    print "Unable to access the '%s' directory." % target
-                    print "Let's try again."
-                    continue
-                else:
-                    print "\nLooks good."
-                    break
-            else:
-                print "\nLooks good."
-                break
-
-        config.add_section('download')
-        config.set('download', 'target', full_target)
-
-        print "\nFinally, do you want to use the same filename format that Yoink! used?"
-        print "See README for the default used otherwise, as well as other formats available."
-        if raw_input("Use Yoink! filename format? [Y/N] ").lower().startswith("y"):
-            config.set('download', 'template_music', "${torrentId}. ${yoinkFormat}")
-            config.set('download', 'template_other', "${torrentId} ${yoinkFormat}")
-        else:
-            # Use defaults, no need to save them in config.
-            pass
-
-        with open(self.config_file, 'w') as f:
-            config.write(f)
-
-        message = """
-Configuration file created successfully.
-
--------------------------------------------------------------------------------
-
-If you plan on adding the script to your cron file, consider using the
-following line:
-
-%s * * * * python %s
-
-The minutes field above has been randomly-determined.
-
-Spreading the scheduling like this helps avoid having a bunch of scripts all
-hitting the server every hour on the hour.
-
-Enjoy!
-""" % (rand_minutes, script_path)
-
-        print message
-        raw_input("Press ENTER to continue... ")
-
-    def _get_accountinfo(self):
-
-        response = self.request('index')
-
-        self.authkey = response['authkey']
-        self.passkey = response['passkey']
-
-    def _login(self):
-
-        data = {'username': self.username,
-                'password': self.password,
-                'keeplogged': 1,
-                'login': "Login"
-        }
-
-        r = self.session.post(WhatFreeGrab.loginpage, data=data, allow_redirects=False)
-        if r.status_code != 302:
-            raise WFGException
-
-        if hasattr(self, 'state'):
-            self.state['cookies'] = self.session.cookies
-            self.save_state()
-
     def add_to_history(self, torrent_id):
-
-        self.history.add(torrent_id)
-
-        self.state['history'] = self.history
+        self.state['history'].add(torrent_id)
         self.save_state()
 
     def create_filename(self, torrent):
@@ -370,7 +152,12 @@ Enjoy!
 
             torrent_id = torrent['torrentId']
 
-            if torrent_id in self.history:
+            if torrent_id in self.state['history']:
+                self.counter['skipped'] += 1
+                continue
+
+            if self.skip_downloads:
+                self.add_to_history(torrent_id)
                 self.counter['skipped'] += 1
                 continue
 
@@ -382,24 +169,21 @@ Enjoy!
                 filepath = filepath[:123] + "~" + filepath[-123:]
 
             if os.path.exists(filepath):
-                self.log.info("File exists for torrent ID %s: '%s'", torrent_id, filepath)
-                self.message("*", newline=False)
+                self.message("* [%s]" % torrent_id)
                 self.add_to_history(torrent_id)
                 self.counter['exists'] += 1
                 continue
 
-            data = self.get_torrent(torrent_id)
+            data = self.what.get_torrent(torrent_id)
             if not data:
-                self.log.info("Error downloading torrent ID %s", torrent_id)
-                self.message("!", newline=False)
+                self.message("! [%s]" % torrent_id)
                 self.counter['error'] += 1
                 continue
 
             with open(filepath, 'wb') as f:
                 f.write(data)
 
-            self.log.info("Downloaded torrent ID %s: '%s'", torrent_id, filepath)
-            self.message("+", newline=False)
+            self.message("+ [%s]" % torrent_id)
             self.counter['downloaded'] += 1
 
             self.add_to_history(torrent_id)
@@ -410,40 +194,16 @@ Enjoy!
 
         params.update(custom_params)
 
-        response = self.request('browse', **params)
+        response = self.what.request('browse', **params)
 
-        for group in response['results']:
+        for group in response['response']['results']:
             if 'torrents' in group:
                 for torrent in group.pop('torrents'):
-                    yoink_format = {
-                        'yoinkFormat':
-                        "%s - %s - %s (%s - %s - %s)" %
-                        (group['artist'][:50], group['groupYear'], group['groupName'][:50],
-                        torrent['media'], torrent['format'], torrent['encoding'])
-                    }
-                    self.torrent_list.append(dict(group.items() + torrent.items() + yoink_format.items()))
+                    self.torrent_list.append(dict(group.items() + torrent.items()))
             else:
-                yoink_format = {'yoinkFormat': group['groupName'][:100]}
-                self.torrent_list.append(dict(group.items() + yoink_format.items()))
+                self.torrent_list.append(dict(group.items()))
 
-        return response['pages']
-
-    def get_torrent(self, torrent_id):
-
-        params = {'action': 'download', 'id': torrent_id}
-
-        if self.authkey:
-            params['authkey'] = self.authkey
-            params['torrent_pass'] = self.passkey
-
-        r = self.session.get(WhatFreeGrab.torrentpage, params=params, allow_redirects=False)
-
-        time.sleep(2) # Be nice.
-
-        if r.status_code == 200 and 'application/x-bittorrent' in r.headers['content-type']:
-            return r.content
-
-        return None
+        return response['response']['pages']
 
     def human_time(self, t):
         # Yes, I know about datetime.datetime, but this was fun.
@@ -467,41 +227,12 @@ Enjoy!
             sys.stdout.flush()
 
     def quit(self, msg, error=False):
-        if error:
-            log = self.log.critical
-        else:
-            log = self.log.info
 
         exec_time = "Script finished in: %s" % self.human_time(time.time() - self.start_time)
-
-        log(msg)
-        log(exec_time)
-        log("-" * 40)
 
         self.message(msg, error)
         self.message(exec_time)
         sys.exit(int(error))
-
-    def request(self, action, **kwargs):
-
-        params = {'action': action}
-
-        if self.authkey:
-            params['auth'] = self.authkey
-
-        params.update(kwargs)
-
-        r = self.session.get(WhatFreeGrab.ajaxpage, params=params, allow_redirects=False)
-
-        time.sleep(2) # Be nice.
-
-        try:
-            json_response = r.json()
-            if json_response['status'] != "success":
-                raise WFGException
-            return json_response['response']
-        except ValueError:
-            raise WFGException
 
     def remove_invalid_chars(self, item):
 
@@ -512,7 +243,6 @@ Enjoy!
     def run(self):
 
         self.message("Building torrent list:", newline=False)
-        self.log.info("Building torrent list")
 
         for params in self.filters:
 
@@ -524,10 +254,12 @@ Enjoy!
                 # just skip this page and we'll catch up on the next run.
                 try:
                     pages = self.get_freeleech(page, params)
-                except WFGException:
+                except whatapi.whatapi.RequestException, e:
+                    self.message("")
+                    self.message("Site error: %s" % e, error=True)
                     pass
-
-                self.message(".", newline=False)
+                else:
+                    self.message(".", newline=False)
 
                 if len(self.torrent_list) > self.max_torrents:
                     self.message("")
@@ -539,19 +271,10 @@ Enjoy!
 
         self.counter['total'] = len(self.torrent_list)
 
-        self.log.info("%s torrents found", self.counter['total'])
-
         self.message("")
         self.message("%s torrents found" % self.counter['total'])
 
-
-        self.log.info("Downloading torrents")
         self.download_torrents()
-
-        self.log.info("%s skipped", self.counter['skipped'])
-        self.log.info("%s exist", self.counter['exists'])
-        self.log.info("%s errors", self.counter['error'])
-        self.log.info("%s downloaded", self.counter['downloaded'])
 
         self.message("")
         self.message("%s skipped" % self.counter['skipped'])
@@ -639,4 +362,4 @@ class SingleInstance:
             sys.exit(-1)
 
 if __name__ == '__main__':
-    WhatFreeGrab(config_file=CONFIG_FILE, state_file=STATE_FILE, lock_file=LOCK_FILE, log_file=LOG_FILE).run()
+    WhatFreeGrab().run()
