b3c72d17a3a16ce4fe3e56e240e7f0a129416c5a
feralhosting <feralhosting@digikool.co.uk>
2014-12-18 09:59:02 +0000

    syncthing

---
+++ Feral Wiki/Software/Syncthing - Basic Setup/scripts/install.syncthing.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+# Script name
+scriptversion="1.0.0"
+scriptname="install.syncthing"
+syncthingversion="0.10.12"
+# Author name
+#
+# Bash Command
+#
+############################
+## Version History Starts ##
+############################
+#
+# How do I customise this updater? 
+# 1: scriptversion="0.0.0" replace "0.0.0" with your script version. This will be shown to the user at the current version.
+# 2: scriptname="somescript" replace "somescript" with your script name. Make it unique to this script.
+# 3: Set the scripturl variable in the variable section to the RAW github URl of the script for updating.
+# 4: Insert your script in the "Script goes here" labelled section
+#
+# This updater deals with updating a single file, the "~/bin/somescript", by updating and switching to this script.
+#
+############################
+### Version History Ends ###
+############################
+#
+############################
+###### Variable Start ######
+############################
+#
+scripturl="https://raw.github.com/feralhosting"
+#
+############################
+####### Variable End #######
+############################
+#
+############################
+#### Self Updater Start ####
+############################
+#
+[[ ! -d ~/bin ]] && mkdir -p ~/bin
+[[ ! -f ~/bin/"$scriptname" ]] && wget -qO ~/bin/"$scriptname" "$scripturl"
+#
+wget -qO ~/.000"$scriptname" "$scripturl"
+#
+if [[ $(sha256sum ~/.000"$scriptname" | awk '{print $1}') != $(sha256sum ~/bin/"$scriptname" | awk '{print $1}') ]]
+then
+    echo -e "#!/bin/bash\nwget -qO ~/bin/$scriptname $scripturl\ncd && rm -f $scriptname{.sh,}\nbash ~/bin/$scriptname\nexit" > ~/.111"$scriptname"
+    bash ~/.111"$scriptname"
+    exit
+else
+    if [[ -z $(ps x | fgrep "bash $HOME/bin/$scriptname" | grep -v grep | head -n 1 | awk '{print $1}') && $(ps x | fgrep "bash $HOME/bin/$scriptname" | grep -v grep | head -n 1 | awk '{print $1}') -ne "$$" ]]
+    then
+        echo -e "#!/bin/bash\ncd && rm -f $scriptname{.sh,}\nbash ~/bin/$scriptname\nexit" > ~/.222"$scriptname"
+        bash ~/.222"$scriptname"
+        exit
+    fi
+fi
+cd && rm -f .{000,111,222}"$scriptname"
+chmod -f 700 ~/bin/"$scriptname"
+#
+############################
+##### Self Updater End #####
+############################
+#
+############################
+#### Core Script Starts ####
+############################
+#
+echo
+echo -e "Hello $(whoami), you have the latest version of the" "\033[36m""$scriptname""\e[0m" "script. This script version is:" "\033[31m""$scriptversion""\e[0m"
+echo
+read -ep "The script has been updated, enter [y] to continue or [q] to exit: " -i "y" updatestatus
+echo
+if [[ "$updatestatus" =~ ^[Yy]$ ]]
+then
+#
+############################
+#### User Script Starts ####
+############################
+#
+    read -ep "What is the current version of syncthing, for example 0.10.12 ?: " -i "$syncthingversion" mainversion
+	wget "https://github.com/syncthing/syncthing/releases/download/v$mainversion/syncthing-linux-amd64-v$mainversion.tar.gz"
+    tar xf ~/syncthing.tar.gz
+    mv ~/syncthing-linux-amd64-v"$syncthingversion"/syncthing ~/bin/
+    cd && rm -rf syncthing{-linux-amd64-v"$syncthingversion",.tar.gz}
+#
+############################
+##### User Script End  #####
+############################
+#
+else
+    echo -e "You chose to exit after updating the scripts."
+    echo
+    cd && bash
+    exit 1
+fi
+#
+############################
+##### Core Script Ends #####
+############################
+#
