be076d7c94f96a95b9fb8f78f06fb6d5162b0cc0
feralhosting <feralhosting@digikool.co.uk>
2015-02-20 22:18:33 +0000

    weechat

--- Feral Wiki/Software/Weechat - IRC client basic setup/scripts/weechat.sh
+++ Feral Wiki/Software/Weechat - IRC client basic setup/scripts/weechat.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # weechat installation
-scriptversion="1.1.0"
+scriptversion="1.1.1"
 scriptname="install.weechat"
 # randomessence
 #
@@ -19,8 +19,8 @@ scriptname="install.weechat"
 ###### Variable Start ######
 ############################
 #
-weechat="http://weechat.org/files/src/weechat-1.0.1.tar.gz"
-weechatfv="1.0.1"
+weechat="http://weechat.org/files/src/weechat-1.1.1.tar.gz"
+weechatfv="1.1.1"
 scripturl="https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Weechat%20-%20IRC%20client%20basic%20setup/scripts/weechat.sh"
 #
 ############################
