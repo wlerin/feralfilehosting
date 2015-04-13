c9b83b9a875e92b74bd3e7e95a1ead78f725d0b2
feralhosting <feralhosting@digikool.co.uk>
2015-04-14 00:10:33 +0100

    multirtru

--- Feral Wiki/Software/Multiple instances - rtorrent and rutorrent/script/install.multirtru.sh
+++ Feral Wiki/Software/Multiple instances - rtorrent and rutorrent/script/install.multirtru.sh
@@ -1,21 +1,49 @@
 #!/bin/bash
-# Install multiple instances of rtorrent and rutorrent
-scriptversion="1.2.2"
-scriptname="install.multirtru"
-# randomessence
+#
+############################
+##### Basic Info Start #####
+############################
+#
+# Script Author: randomessence
+#
+# Script Contributors: 
+#
+# License: This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License. https://creativecommons.org/licenses/by-sa/4.0/
+#
+# Bash Command for easy reference:
 #
 # wget -qO ~/install.multirtru.sh http://git.io/_zVu0A && bash ~/install.multirtru.sh
+#
+############################
+###### Basic Info End ######
+############################
+#
+############################
+#### Script Notes Start ####
+############################
+#
+##
+#
+############################
+##### Script Notes End #####
+############################
 #
 ############################
 ## Version History Starts ##
 ############################
 #
-# v1.1.6 random password option.
-# v1.1.5 more tweaks and fixed loop.
-# v1.1.3 small tweaks to instalaltion script
-# v1.1.2 template updated
-# v1.2.0 autodl custom installation included
-# v1.2.1 hostname -f
+if [[ ! -z $1 && $1 == 'changelog' ]]; then echo
+    #
+    echo 'v1.1.6 random password option'
+    echo 'v1.1.5 more tweaks and fixed loop.'
+    echo 'v1.1.3 small tweaks to installation script'
+    echo 'v1.1.2 template updated'
+    echo 'v1.2.0 autodl custom installation included'
+    echo 'v1.2.1 hostname -f'
+    #
+    echo
+    exit
+fi
 #
 ############################
 ### Version History Ends ###
@@ -25,48 +53,188 @@ scriptname="install.multirtru"
 ###### Variable Start ######
 ############################
 #
+# Script Version number is set here.
+scriptversion="1.2.2"
+#
+# Script name goes here. Please prefix with install.
+scriptname="install.multirtru"
+#
+# Author name goes here.
+scriptauthor="randomessence"
+#
+# Contributor's names go here.
+contributors="None credited"
+#
+# Set the http://git.io/ shortened URL for the raw github URL here:
+gitiourl="http://git.io/_zVu0A"
+#
+# Don't edit: This is the bash command shown when using the info option.
+gitiocommand="wget -qO ~/$scriptname $gitiourl && bash ~/$scriptname"
+#
+# This is the raw github url of the script to use with the built in updater.
+scripturl="https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Multiple%20instances%20-%20rtorrent%20and%20rutorrent/script/install.multirtru.sh"
+#
+# This will generate a random port for the script between the range 10001 to 49999 to use with applications. You can ignore this unless needed.
+appport=$(shuf -i 10001-49999 -n 1)
+#
+# This wil take the previously generated port and test it to make sure it is not in use, generating it again until it has selected an open port.
+while [[ "$(netstat -ln | grep ':'"$appport"'' | grep -c 'LISTEN')" -eq "1" ]]
+do
+    appport=$(shuf -i 10001-49999 -n 1)
+done
+#
+############################
+## Custom Variables Start ##
+############################
+#
+# Random password for use with rutorrent user creation
 randompass=$(< /dev/urandom tr -dc '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' | head -c20; echo;)
+#
+# autodl configuration passwords
+pass=$(< /dev/urandom tr -dc '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' | head -c20; echo;)
+#
+# Some basic plugins
 feralstats="https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Installable%20software/Feralstats%20plugin%20for%20ruTorrent/files/feralstats.zip"
 ratiocolor="https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Other%20software/Rutorrent%20-%20Colored%20Ratio%20Column%20Plugin/ratiocolor-1/ratiocolor.zip"
+#
+# rtorrent.rc template
 confurl="https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Multiple%20instances%20-%20rtorrent%20and%20rutorrent/conf/.rtorrent.rc"
-scripturl="https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Multiple%20instances%20-%20rtorrent%20and%20rutorrent/script/install.multirtru.sh"
 #
 autodlirssicommunity="http://update.autodl-community.com/autodl-irssi-community.zip"
 autodltrackers="http://update.autodl-community.com/autodl-trackers.zip"
+#
 # URL for autodl-rutorrent
 autodlrutorrent="https://github.com/autodl-community/autodl-rutorrent/archive/master.zip"
-# Uses shuf to pick a random port between 6000 and 50000
-port=$(shuf -i 10000-50000 -n 1)
-# Random password generation
-pass=$(< /dev/urandom tr -dc '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' | head -c20; echo;)
+#
+############################
+### Custom Variables End ###
+############################
+#
+# Disables the built in script updater permanently by setting this variable to 0.
+updaterenabled="1"
+#
 ############################
 ####### Variable End #######
+############################
+#
+############################
+#### Script Info Starts ####
+############################
+#
+# Use this to show a user script information when they use the info option with the script.
+if [[ ! -z $1 && $1 == 'info' ]]
+then
+    echo
+    echo -e "\033[32m""Script Details:""\e[0m"
+    echo
+    echo "Script version: $scriptversion"
+    echo
+    echo "Script Author: $scriptauthor"
+    echo
+    echo "Script Contributors: $contributors"
+    echo
+    echo -e "\033[32m""Script Information and usage instructions:""\e[0m"
+    echo
+    #
+    ###################################
+    #### Custom Script Notes Start ####
+    ###################################
+    #
+    echo -e "Put your instructions or script information here using echoes"
+    #
+    ###################################
+    ##### Custom Script Notes End #####
+    ###################################
+    #
+    echo
+    echo -e "\033[32m""Script options:""\e[0m"
+    echo
+    echo -e "\033[36mchangelog\e[0m = See the version history and change log of this script."
+    echo
+    echo -e "Example usage: \033[36m$scriptname changelog\e[0m"
+    echo
+    echo -e "\033[36minfo\e[0m = Show the script information and usage instructions."
+    echo
+    echo -e "Example usage: \033[36m$scriptname info\e[0m"
+    echo
+    echo -e "\033[31mImportant note:\e[0m Options \033[36mqr\e[0m and \033[36mnu\e[0m are interchangeable and usable together."
+    echo
+    echo -e "For example: \033[36m$scriptname qr nu\e[0m or \033[36m$scriptname nu qr\e[0m will both work"
+    echo
+    echo -e "\033[36mqr\e[0m = Quick Run - use this to bypass the default update prompts and run the main script directly."
+    echo
+    echo -e "Example usage: \033[36m$scriptname qr\e[0m"
+    echo
+    echo -e "\033[36mnu\e[0m = No Update - disable the built in updater. Useful for testing new features or debugging."
+    echo
+    echo -e "Example usage: \033[36m$scriptname nu\e[0m"
+    echo
+    echo -e "\033[32mBash Commands:\e[0m"
+    echo
+    echo -e "$gitiocommand"
+    echo
+    echo -e "~/bin/$scriptname"
+    echo
+    echo -e "$scriptname"
+    #
+    echo
+    exit
+fi
+#
+############################
+##### Script Info Ends #####
 ############################
 #
 ############################
 #### Self Updater Start ####
 ############################
 #
-[[ ! -d ~/bin ]] && mkdir -p ~/bin
-[[ ! -f ~/bin/"$scriptname" ]] && wget -qO ~/bin/"$scriptname" "$scripturl"
+# Quick Run option part 1: If qr is used it will create this file. Then if the script also updates, whihc woudl reset the option, it will then find this file and set it back.
+if [[ ! -z $1 && $1 == 'qr' ]] || [[ ! -z $2 && $2 == 'qr' ]];then echo -n '' > ~/.quickrun; fi
 #
-wget -qO ~/.000"$scriptname" "$scripturl"
-#
-if [[ $(sha256sum ~/.000"$scriptname" | awk '{print $1}') != $(sha256sum ~/bin/"$scriptname" | awk '{print $1}') ]]
+# No Update option: This disables the updater features if the script option "nu" was used when running the script.
+if [[ ! -z $1 && $1 == 'nu' ]] || [[ ! -z $2 && $2 == 'nu' ]]
 then
-    echo -e "#!/bin/bash\nwget -qO ~/bin/$scriptname $scripturl\ncd && rm -f $scriptname{.sh,}\nbash ~/bin/$scriptname\nexit" > ~/.111"$scriptname"
-    bash ~/.111"$scriptname"
-    exit
+    echo
+    echo "The Updater has been temporarily disabled"
+    echo
+    scriptversion="$scriptversion-nu"
 else
-    if [[ -z $(ps x | fgrep "bash $HOME/bin/$scriptname" | grep -v grep | head -n 1 | awk '{print $1}') && $(ps x | fgrep "bash $HOME/bin/$scriptname" | grep -v grep | head -n 1 | awk '{print $1}') -ne "$$" ]]
+    #
+    # Check to see if the variable "updaterenabled" is set to 1. If it is set to 0 the script will bypass the built in updater regardless of the options used.
+    if [[ "$updaterenabled" -eq 1 ]]
     then
-        echo -e "#!/bin/bash\ncd && rm -f $scriptname{.sh,}\nbash ~/bin/$scriptname\nexit" > ~/.222"$scriptname"
-        bash ~/.222"$scriptname"
-        exit
+        [[ ! -d ~/bin ]] && mkdir -p ~/bin
+        [[ ! -f ~/bin/"$scriptname" ]] && wget -qO ~/bin/"$scriptname" "$scripturl"
+        #
+        wget -qO ~/.000"$scriptname" "$scripturl"
+        #
+        if [[ $(sha256sum ~/.000"$scriptname" | awk '{print $1}') != $(sha256sum ~/bin/"$scriptname" | awk '{print $1}') ]]
+        then
+            echo -e "#!/bin/bash\nwget -qO ~/bin/$scriptname $scripturl\ncd && rm -f $scriptname{.sh,}\nbash ~/bin/$scriptname\nexit" > ~/.111"$scriptname"
+            bash ~/.111"$scriptname"
+            exit
+        else
+            if [[ -z $(ps x | fgrep "bash $HOME/bin/$scriptname" | grep -v grep | head -n 1 | awk '{print $1}') && $(ps x | fgrep "bash $HOME/bin/$scriptname" | grep -v grep | head -n 1 | awk '{print $1}') -ne "$$" ]]
+            then
+                echo -e "#!/bin/bash\ncd && rm -f $scriptname{.sh,}\nbash ~/bin/$scriptname\nexit" > ~/.222"$scriptname"
+                bash ~/.222"$scriptname"
+                exit
+            fi
+        fi
+        cd && rm -f .{000,111,222}"$scriptname"
+        chmod -f 700 ~/bin/"$scriptname"
+        echo
+    else
+        echo
+        echo "The Updater has been disabled"
+        echo
+        scriptversion="$scriptversion-DEV"
     fi
 fi
-cd && rm -f .{000,111,222}"$scriptname"
-chmod -f 700 ~/bin/"$scriptname"
+#
+# Quick Run option part 2: If quick run was set and the updater section completes this will enable quick run again then remove the file.
+if [[ -f ~/.quickrun ]];then updatestatus="y"; rm -f ~/.quickrun; fi
 #
 ############################
 ##### Self Updater End #####
@@ -76,13 +244,19 @@ chmod -f 700 ~/bin/"$scriptname"
 #### Core Script Starts ####
 ############################
 #
-echo
-echo -e "Hello $(whoami), you have the latest version of the" "\033[36m""$scriptname""\e[0m" "script. This script version is:" "\033[31m""$scriptversion""\e[0m"
-echo
-read -ep "The scripts have been updated, do you wish to continue [y] or exit now [q] : " -i "y" updatestatus
-echo
+if [[ "$updatestatus" == "y" ]]
+then
+    :
+else
+    echo -e "Hello $(whoami), you have the latest version of the" "\033[36m""$scriptname""\e[0m" "script. This script version is:" "\033[31m""$scriptversion""\e[0m"
+    echo
+    read -ep "The script has been updated, enter [y] to continue or [q] to exit: " -i "y" updatestatus
+    echo
+fi
+#
 if [[ "$updatestatus" =~ ^[Yy]$ ]]
 then
+#
 ############################
 #### User Script Starts ####
 ############################
@@ -138,19 +312,19 @@ then
         if [[ -d ~/.irssi-"$suffix" ]]
         then
             rm -rf ~/.irssi-"$suffix"
-            echo "~/.irssi-"$suffix" has been removed"
+            echo "~/.irssi-$suffix has been removed"
             echo
         else
-            echo "~/.irssi-"$suffix" not found, skipping"
+            echo "~/.irssi-$suffix not found, skipping"
             echo
         fi
         if [[ -d ~/.autodl-"$suffix" ]]
         then
             rm -rf ~/.autodl-"$suffix"
-            echo "~/.autodl-"$suffix" has been removed"
+            echo "~/.autodl-$suffix has been removed"
             echo
         else
-            echo "~/.autodl-"$suffix" not found, skipping"
+            echo "~/.autodl-$suffix not found, skipping"
             echo
         fi
         #
@@ -249,7 +423,7 @@ then
                 # Delete files we no longer need.
                 rm -f ~/autodl-{irssi,trackers}.zip ~/.irssi-"$suffix"/scripts/{README*,CONTRIBUTING.md,autodl-irssi.pl}
                 echo "Writing configuration files"
-                echo -e "[options]\ngui-server-port = $port\ngui-server-password = $pass" > ~/.autodl-"$suffix"/autodl.cfg
+                echo -e "[options]\ngui-server-port = $appport\ngui-server-password = $pass" > ~/.autodl-"$suffix"/autodl.cfg
                 #
                 ############################
                 ######## Autodl End ########
@@ -269,7 +443,7 @@ then
                 # Delete the downloaded zip and the unpacked folder we no longer require.
                 cd && rm -rf autodl-rutorrent{-master,.zip}
                 # Uses echo to make the config file for the rutorrent plugun to work with autodl using the variables port and pass
-                echo -ne '<?php\n$autodlPort = '"$port"';\n$autodlPassword = "'"$pass"'";\n?>' > ~/www/$(whoami).$(hostname -f)/public_html/rutorrent-"$suffix"/plugins/autodl-irssi/conf.php
+                echo -ne '<?php\n$autodlPort = '"$appport"';\n$autodlPassword = "'"$pass"'";\n?>' > ~/www/$(whoami).$(hostname -f)/public_html/rutorrent-"$suffix"/plugins/autodl-irssi/conf.php
                 #
                 ############################
                 ###### RuTorrent Ends ######
@@ -296,7 +470,7 @@ then
                 ###### Fix script End ######
                 ############################
                 #
-                screen -dmS autodl-"$suffix" irssi --home=$HOME/.irssi-"$suffix"/
+                screen -dmS autodl-"$suffix" irssi --home="$HOME"/.irssi-"$suffix"/
                 echo 'screen -dmS autodl-'"$suffix"' irssi --home=$HOME/.irssi-'"$suffix"'/' >> ~/multirtru.restart.txt
                 # Send a command to the new screen telling Autodl to update itself. This basically generates the ~/.autodl/AutodlState.xml files with updated info.
                 screen -S autodl-"$suffix" -p 0 -X stuff '/autodl update^M'
@@ -363,7 +537,7 @@ then
                 echo -e "\e[0m"
                 echo -e "You can attach to the screen using this command:"
                 echo
-                echo -e "\033[32m""screen -r autodl-"$suffix"""\e[0m"
+                echo -e "\033[32m""screen -r autodl-$suffix""\e[0m"
                 echo
             fi
             echo -e "\033[32m""4:""\e[0m" "Creating the screen process"
@@ -461,7 +635,7 @@ else
     echo -e "You chose to exit after updating the scripts."
     echo
     cd && bash
-    exit 1
+    exit
 fi
 #
 ############################
