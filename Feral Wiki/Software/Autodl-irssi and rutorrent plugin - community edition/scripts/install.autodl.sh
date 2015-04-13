2b28bd74eee22006713e918cef98f4cc59639368
feralhosting <feralhosting@digikool.co.uk>
2015-04-13 21:55:24 +0100

    autodl

--- Feral Wiki/Software/Autodl-irssi and rutorrent plugin - community edition/scripts/install.autodl.sh
+++ Feral Wiki/Software/Autodl-irssi and rutorrent plugin - community edition/scripts/install.autodl.sh
@@ -12,7 +12,7 @@
 #
 # Bash Command for easy reference:
 #
-# wget -qO ~/install.autodl http://git.io/oTUCMg && bash ~/install.autodl qr
+# wget -qO ~/install.autodl http://git.io/oTUCMg && bash ~/install.autodl
 #
 ############################
 ###### Basic Info End ######
@@ -30,34 +30,41 @@
 ## Version History Starts ##
 ############################
 #
-# v1.4.5 - Template
-# v1.4.4 - Template
-# v1.4.3 - Template
-# v1.4.2 - Template
-# v1.4.1 - Visual tweaks.
-# v1.4.0 - better isolating of screen PIDS
-# v1.3.9 - test for existing but empty autodl.cfg
-# v1.3.8 - template
-# v1.3.6 - $(hostname -f) *points fingers*
-# v1.3.5 - does not force ruTorrent and will install and configure autodl without it.
-# v1.3.4 - update URLs changed to http://update.autodl-community.com
-# v1.3.3 - small tweaks
-# v1.3.2 - confusion = automation + comments
-# v1.3.1 - updater template merged
-# v1.3.0 - URLs updated as google code is being phased out.
-# v1.2.9 - modernised
-# v1.2.8 - removes autodl files before installing. Clean install. No longer overwrites existing autodl.cfg if present when reinstalling
-# v1.2.7 - also gets and installs most current tracker list
-# v1.2.6 - random pass generated and inserted but still requires user confirmation.
-# v1.2.5 - general tidy up and less verbose output
-# v1.2.4 - merged fix script
-# v1.2.3 - Added disclaimer
-# v1.2.2 - Fixed mv steps, dir struct did in fact change by using the release builds
-# v1.2.1 - Code cleanup (cleaner var names, better structure)...more commenting
-# v1.2 - Updated plugin to community edition, freeleech flag and a few other extras
-# v1.1 - Set the password prompt to tell you not to use spaces, fixed a typo and made sure to set svn to --quiet. Groundwork for version checking
-# v1.0.1 - Finish ruTorrent check mechanism -- checks for ruTorrent folder in vhost
-# v1.0 - Initial release
+if [[ ! -z $1 && $1 == 'changelog' ]]; then echo
+    #
+    echo 'v1.4.6 - Template and minor tweaks.'
+    echo 'v1.4.5 - Template.'
+    echo 'v1.4.4 - Template.'
+    echo 'v1.4.3 - Template.'
+    echo 'v1.4.2 - Template.'
+    echo 'v1.4.1 - Visual tweaks.'
+    echo 'v1.4.0 - better isolating of screen PIDS.'
+    echo 'v1.3.9 - test for existing but empty autodl.cfg.'
+    echo 'v1.3.8 - template.'
+    echo 'v1.3.6 - $(hostname -f) *points fingers*.'
+    echo 'v1.3.5 - does not force ruTorrent and will install and configure autodl without it.'
+    echo 'v1.3.4 - update URLs changed to http://update.autodl-community.com.'
+    echo 'v1.3.3 - small tweaks.'
+    echo 'v1.3.2 - confusion = automation + comments.'
+    echo 'v1.3.1 - updater template merged.'
+    echo 'v1.3.0 - URLs updated as google code is being phased out.'
+    echo 'v1.2.9 - modernised.'
+    echo 'v1.2.8 - removes autodl files before installing. Clean install. No longer overwrites existing autodl.cfg if present when reinstalling.'
+    echo 'v1.2.7 - also gets and installs most current tracker list.'
+    echo 'v1.2.6 - random pass generated and inserted but still requires user confirmation.'
+    echo 'v1.2.5 - general tidy up and less verbose output.'
+    echo 'v1.2.4 - merged fix script.'
+    echo 'v1.2.3 - Added disclaimer.'
+    echo 'v1.2.2 - Fixed mv steps, dir struct did in fact change by using the release builds.'
+    echo 'v1.2.1 - Code cleanup (cleaner var names, better structure)...more commenting.'
+    echo 'v1.2 - Updated plugin to community edition, freeleech flag and a few other extras.'
+    echo 'v1.1 - Set the password prompt to tell you not to use spaces, fixed a typo and made sure to set svn to --quiet. Groundwork for version checking.'
+    echo 'v1.0.1 - Finish ruTorrent check mechanism -- checks for ruTorrent folder in vhost.'
+    echo 'v1.0 - Initial release.'
+    #
+    echo
+    exit
+fi
 #
 ############################
 ### Version History Ends ###
@@ -68,7 +75,7 @@
 ############################
 #
 # Script Version number is set here.
-scriptversion="1.4.5"
+scriptversion="1.4.6"
 #
 # Script name goes here. Please prefix with install.
 scriptname="install.autodl"
@@ -92,12 +99,12 @@ autodltrackers="http://update.autodl-community.com/autodl-trackers.zip"
 autodlrutorrent="https://github.com/autodl-community/autodl-rutorrent/archive/master.zip"
 #
 # Uses shuf to pick a random port between 10001 and 49999
-port=$(shuf -i 10001-49999 -n 1)
+appport=$(shuf -i 10001-49999 -n 1)
 #
 # This wil take the previously generated port and test it to make sure it is not in use, generating it again until it has selected an open port.
-while [[ "$(netstat -ln | grep ':'"$port"'' | grep -c 'LISTEN')" -eq "1" ]]
+while [[ "$(netstat -ln | grep ':'"$appport"'' | grep -c 'LISTEN')" -eq "1" ]]
 do
-    port=$(shuf -i 10001-49999 -n 1)
+    appport=$(shuf -i 10001-49999 -n 1)
 done
 #
 # Random password generation
@@ -132,19 +139,35 @@ then
     echo
     echo -e "\033[32m""Script Details:""\e[0m"
     echo
+    echo "Script version: $scriptversion"
+    echo
     echo "Script Author: $scriptauthor"
     echo
     echo "Script Contributors: $contributors"
     echo
-    #
-    ################# Custom Script Notes #################
-    echo -e "\033[32m""Script Notes - this script will do these things:""\e[0m"
+    echo -e "\033[32m""Script Information and usage instructions:""\e[0m"
     echo
-    echo "Put your instructions or script information here"
-    echo
-    #######################################################
     #
+    ###################################
+    #### Custom Script Notes Start ####
+    ###################################
+    #
+    echo -e "Put your instructions or script information here using echoes"
+    #
+    ###################################
+    ##### Custom Script Notes End #####
+    ###################################
+    #
+    echo
     echo -e "\033[32m""Script options:""\e[0m"
+    echo
+    echo -e "\033[36mchangelog\e[0m = See the version history and change log of this script."
+    echo
+    echo -e "Example usage: \033[36m$scriptname changelog\e[0m"
+    echo
+    echo -e "\033[36minfo\e[0m = Show the script information and usage instructions."
+    echo
+    echo -e "Example usage: \033[36m$scriptname info\e[0m"
     echo
     echo -e "\033[31mImportant note:\e[0m Options \033[36mqr\e[0m and \033[36mnu\e[0m are interchangeable and usable together."
     echo
@@ -178,15 +201,19 @@ fi
 #### Self Updater Start ####
 ############################
 #
+# Quick Run option part 1: If qr is used it will create this file. Then if the script also updates, whihc woudl reset the option, it will then find this file and set it back.
 if [[ ! -z $1 && $1 == 'qr' ]] || [[ ! -z $2 && $2 == 'qr' ]];then echo -n '' > ~/.quickrun; fi
 #
+# No Update option: This disables the updater features if the script option "nu" was used when running the script.
 if [[ ! -z $1 && $1 == 'nu' ]] || [[ ! -z $2 && $2 == 'nu' ]]
 then
     echo
     echo "The Updater has been temporarily disabled"
     echo
-    scriptversion=""$scriptversion"-nu"
+    scriptversion="$scriptversion-nu"
 else
+    #
+    # Check to see if the variable "updaterenabled" is set to 1. If it is set to 0 the script will bypass the built in updater regardless of the options used.
     if [[ "$updaterenabled" -eq 1 ]]
     then
         [[ ! -d ~/bin ]] && mkdir -p ~/bin
@@ -214,10 +241,11 @@ else
         echo
         echo "The Updater has been disabled"
         echo
-        scriptversion=""$scriptversion"-DEV"
+        scriptversion="$scriptversion-DEV"
     fi
 fi
 #
+# Quick Run option part 2: If quick run was set and the updater section completes this will enable quick run again then remove the file.
 if [[ -f ~/.quickrun ]];then updatestatus="y"; rm -f ~/.quickrun; fi
 #
 ############################
@@ -282,20 +310,20 @@ then
     # Delete files we no longer need.
     rm -f ~/autodl-{irssi,trackers}.zip ~/.irssi/scripts/{README*,CONTRIBUTING.md,autodl-irssi.pl}
     # If the autodl.cfg exists we use sed to update the existing username and pass with the ones the script has generated.
-    # else we use and echo to create our autodl.cfg file. Takes the two previously made variables, $port and $pass to update/create the required info.
+    # else we use and echo to create our autodl.cfg file. Takes the two previously made variables, $appport and $pass to update/create the required info.
     if [[ -f ~/.autodl/autodl.cfg ]]
     then
         if [[ $(tr -d "\r\n" < ~/.autodl/autodl.cfg | wc -c) -eq 0 ]]
         then
-            echo -e "[options]\ngui-server-port = $port\ngui-server-password = $pass" > ~/.autodl/autodl.cfg
+            echo -e "[options]\ngui-server-port = $appport\ngui-server-password = $pass" > ~/.autodl/autodl.cfg
         else
             # Sed command to enter the port variable
-            sed -ri 's|(.*)gui-server-port =(.*)|gui-server-port = '"$port"'|g' ~/.autodl/autodl.cfg
+            sed -ri 's|(.*)gui-server-port =(.*)|gui-server-port = '"$appport"'|g' ~/.autodl/autodl.cfg
             # Sed command to enter the password variable
             sed -ri 's|(.*)gui-server-password =(.*)|gui-server-password = '"$pass"'|g' ~/.autodl/autodl.cfg
         fi
     else 
-        echo -e "[options]\ngui-server-port = $port\ngui-server-password = $pass" > ~/.autodl/autodl.cfg
+        echo -e "[options]\ngui-server-port = $appport\ngui-server-password = $pass" > ~/.autodl/autodl.cfg
     fi
     #
     ############################
@@ -319,7 +347,7 @@ then
         # Delete the downloaded zip and the unpacked folder we no longer require.
         cd && rm -rf autodl-rutorrent{-master,.zip}
         # Uses echo to make the config file for the rutorrent plugun to work with autodl using the variables port and pass
-        echo -ne '<?php\n$autodlPort = '"$port"';\n$autodlPassword = "'"$pass"'";\n?>' > ~/www/$(whoami).$(hostname -f)/public_html/rutorrent/plugins/autodl-irssi/conf.php
+        echo -ne '<?php\n$autodlPort = '"$appport"';\n$autodlPassword = "'"$pass"'";\n?>' > ~/www/$(whoami).$(hostname -f)/public_html/rutorrent/plugins/autodl-irssi/conf.php
         #
         ############################
         ###### RuTorrent Ends ######
