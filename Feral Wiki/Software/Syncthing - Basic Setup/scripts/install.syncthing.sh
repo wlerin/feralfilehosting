d6d2bc5e95964067642d5bef61f8473b672f1e5f
feralhosting <feralhosting@digikool.co.uk>
2015-04-14 10:36:46 +0100

    syncthing

--- Feral Wiki/Software/Syncthing - Basic Setup/scripts/install.syncthing.sh
+++ Feral Wiki/Software/Syncthing - Basic Setup/scripts/install.syncthing.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-# Install Syncthing
-scriptversion="1.0.5"
-scriptname="install.syncthing"
-syncthingversion="0.10.29"
-# randomessence
+#
+############################
+##### Basic Info Start #####
+############################
+#
+# Script Author: randomessence
+#
+# Script Contributors: 
+#
+# License: This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License. https://creativecommons.org/licenses/by-sa/4.0/
+#
+# Bash Command for easy reference:
 #
 # wget -qO ~/install.syncthing http://git.io/-MNlxQ && bash ~/install.syncthing
+#
+############################
+###### Basic Info End ######
+############################
 #
 ############################
 #### Script Notes Start ####
@@ -21,6 +32,25 @@ syncthingversion="0.10.29"
 ## Version History Starts ##
 ############################
 #
+if [[ ! -z $1 && $1 == 'changelog' ]]; then echo
+    #
+    # put your version changes in the single quotes and then uncomment the line.
+    #
+    #echo 'v0.1.0 - My changes go here'
+    #echo 'v0.0.9 - My changes go here'
+    #echo 'v0.0.8 - My changes go here'
+    #echo 'v0.0.7 - My changes go here'
+    #echo 'v0.0.6 - My changes go here'
+    #echo 'v0.0.5 - My changes go here'
+    #echo 'v0.0.4 - My changes go here'
+    #echo 'v0.0.3 - My changes go here'
+    #echo 'v0.0.2 - My changes go here'
+    echo 'v1.0.6 - Updater template updated - script detects latest version automatically.'
+    #
+    echo
+    exit
+fi
+#
 ############################
 ### Version History Ends ###
 ############################
@@ -29,34 +59,162 @@ syncthingversion="0.10.29"
 ###### Variable Start ######
 ############################
 #
+# Script Version number is set here.
+scriptversion="1.0.6"
+#
+# Script name goes here. Please prefix with install.
+scriptname="install.syncthing"
+#
+# Author name goes here.
+scriptauthor="randomessence"
+#
+# Contributor's names go here.
+contributors="None credited"
+#
+# Set the http://git.io/ shortened URL for the raw github URL here:
+gitiourl="http://git.io/-MNlxQ"
+#
+# Don't edit: This is the bash command shown when using the info option.
+gitiocommand="wget -qO ~/$scriptname $gitiourl && bash ~/$scriptname"
+#
+# This is the raw github url of the script to use with the built in updater.
 scripturl="https://raw.githubusercontent.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Syncthing%20-%20Basic%20Setup/scripts/install.syncthing.sh"
 #
+# This will generate a 20 character random passsword for use with your applications.
+apppass=$(< /dev/urandom tr -dc '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' | head -c20; echo;)
+# This will generate a random port for the script between the range 10001 to 49999 to use with applications. You can ignore this unless needed.
+appport=$(shuf -i 10001-49999 -n 1)
+#
+# This wil take the previously generated port and test it to make sure it is not in use, generating it again until it has selected an open port.
+while [[ "$(netstat -ln | grep ':'"$appport"'' | grep -c 'LISTEN')" -eq "1" ]]; do appport=$(shuf -i 10001-49999 -n 1); done
+#
+# Script user's http www URL in the format http://username.server.feralhosting.com/
+host1http="http://$(whoami).$(hostname -f)/"
+# Script user's https www URL in the format https://username.server.feralhosting.com/
+host1https="https://$(whoami).$(hostname -f)/"
+# Script user's http www url in the format https://server.feralhosting.com/username/
+host2http="http://$(hostname -f)/$(whoami)/"
+# Script user's https www url in the format https://server.feralhosting.com/username/
+host2https="https://$(hostname -f)/$(whoami)/"
+#
+# feralwww - sets the full path to the default public_html directory if it exists.
+[[ -d ~/www/$(whoami).$(hostname -f)/public_html ]] && feralwww="$HOME/www/$(whoami).$(hostname -f)/public_html/"
+# rtorrentdata - sets the full path to the rtorrent data directory if it exists.
+[[ -d ~/private/rtorrent/data ]] && rtorrentdata="$HOME/private/rtorrent/data"
+# deluge - sets the full path to the deluge data directory if it exists.
+[[ -d ~/private/deluge/data ]] && delugedata="$HOME/private/deluge/data"
+# transmission - sets the full path to the transmission data directory if it exists.
+[[ -d ~/private/transmission/data ]] && transmissiondata="$HOME/private/transmission/data"
+#
 ############################
-####### Variable End #######
+## Custom Variables Start ##
 ############################
 #
-# Disables the built in script updater permanently.
-updaterenabled="1"
+syncthingversion="$(wget -q -O- https://github.com/syncthing/syncthing/releases/latest |  grep '<strong>syncthing-linux-amd64-v' | sed -rn 's|(.*)<strong>syncthing-linux-amd64-v(.*).tar.gz</strong>|\2|p')"
+syncthingurl="https://github.com/syncthing/syncthing/releases/download/v$syncthingversion/syncthing-linux-amd64-v$syncthingversion.tar.gz"
 #
-guiport=$(shuf -i 10001-49999 -n 1)
-syncport=$(expr 1 + $guiport)
+syncport=$(expr 1 + $appport)
+while [[ "$(netstat -ln | grep ':'"$syncport"'' | grep -c 'LISTEN')" -eq "1" ]]; do syncport=$(shuf -i 10001-49999 -n 1); done
 #
 apacheconf="http://git.io/WqUqBQ"
 nginxconf="http://git.io/TnhqpA"
 #
 ############################
+### Custom Variables End ###
+############################
+#
+# Disables the built in script updater permanently by setting this variable to 0.
+updaterenabled="1"
+#
+############################
+####### Variable End #######
+############################
+#
+############################
+#### Script Info Starts ####
+############################
+#
+# Use this to show a user script information when they use the info option with the script.
+if [[ ! -z $1 && $1 == 'info' ]]
+then
+    echo
+    echo -e "\033[32m""Script Details:""\e[0m"
+    echo
+    echo "Script version: $scriptversion"
+    echo
+    echo "Script Author: $scriptauthor"
+    echo
+    echo "Script Contributors: $contributors"
+    echo
+    echo -e "\033[32m""Script Information and usage instructions:""\e[0m"
+    echo
+    #
+    ###################################
+    #### Custom Script Notes Start ####
+    ###################################
+    #
+    echo -e "Put your instructions or script information here using echoes"
+    #
+    ###################################
+    ##### Custom Script Notes End #####
+    ###################################
+    #
+    echo
+    echo -e "\033[32m""Script options:""\e[0m"
+    echo
+    echo -e "\033[36mchangelog\e[0m = See the version history and change log of this script."
+    echo
+    echo -e "Example usage: \033[36m$scriptname changelog\e[0m"
+    echo
+    echo -e "\033[36minfo\e[0m = Show the script information and usage instructions."
+    echo
+    echo -e "Example usage: \033[36m$scriptname info\e[0m"
+    echo
+    echo -e "\033[31mImportant note:\e[0m Options \033[36mqr\e[0m and \033[36mnu\e[0m are interchangeable and usable together."
+    echo
+    echo -e "For example: \033[36m$scriptname qr nu\e[0m or \033[36m$scriptname nu qr\e[0m will both work"
+    echo
+    echo -e "\033[36mqr\e[0m = Quick Run - use this to bypass the default update prompts and run the main script directly."
+    echo
+    echo -e "Example usage: \033[36m$scriptname qr\e[0m"
+    echo
+    echo -e "\033[36mnu\e[0m = No Update - disable the built in updater. Useful for testing new features or debugging."
+    echo
+    echo -e "Example usage: \033[36m$scriptname nu\e[0m"
+    echo
+    echo -e "\033[32mBash Commands:\e[0m"
+    echo
+    echo -e "$gitiocommand"
+    echo
+    echo -e "~/bin/$scriptname"
+    echo
+    echo -e "$scriptname"
+    #
+    echo
+    exit
+fi
+#
+############################
+##### Script Info Ends #####
+############################
+#
+############################
 #### Self Updater Start ####
 ############################
 #
+# Quick Run option part 1: If qr is used it will create this file. Then if the script also updates, whihc woudl reset the option, it will then find this file and set it back.
 if [[ ! -z $1 && $1 == 'qr' ]] || [[ ! -z $2 && $2 == 'qr' ]];then echo -n '' > ~/.quickrun; fi
 #
+# No Update option: This disables the updater features if the script option "nu" was used when running the script.
 if [[ ! -z $1 && $1 == 'nu' ]] || [[ ! -z $2 && $2 == 'nu' ]]
 then
     echo
     echo "The Updater has been temporarily disabled"
     echo
-    scriptversion=""$scriptversion"-nu"
+    scriptversion="$scriptversion-nu"
 else
+    #
+    # Check to see if the variable "updaterenabled" is set to 1. If it is set to 0 the script will bypass the built in updater regardless of the options used.
     if [[ "$updaterenabled" -eq 1 ]]
     then
         [[ ! -d ~/bin ]] && mkdir -p ~/bin
@@ -84,10 +242,11 @@ else
         echo
         echo "The Updater has been disabled"
         echo
-        scriptversion=""$scriptversion"-DEV"
+        scriptversion="$scriptversion-DEV"
     fi
 fi
 #
+# Quick Run option part 2: If quick run was set and the updater section completes this will enable quick run again then remove the file.
 if [[ -f ~/.quickrun ]];then updatestatus="y"; rm -f ~/.quickrun; fi
 #
 ############################
@@ -104,8 +263,6 @@ then
 else
     echo -e "Hello $(whoami), you have the latest version of the" "\033[36m""$scriptname""\e[0m" "script. This script version is:" "\033[31m""$scriptversion""\e[0m"
     echo
-    echo -e "The version of the" "\033[33m""Syncthing""\e[0m" "being used in this script is:" "\033[31m""$syncthingversion""\e[0m"
-    echo
     read -ep "The script has been updated, enter [y] to continue or [q] to exit: " -i "y" updatestatus
     echo
 fi
@@ -117,23 +274,21 @@ then
 #### User Script Starts ####
 ############################
 #
-    echo "https://github.com/syncthing/syncthing/releases/latest"
+    echo "Downloading and installing v$syncthingversion of syncthing"
     echo
-    read -ep "What is the version of syncthing you want to install? (change version here if needed then press enter): " -i "$syncthingversion" mainversion
-    echo
-	wget -qO ~/syncthing.tar.gz "https://github.com/syncthing/syncthing/releases/download/v$mainversion/syncthing-linux-amd64-v$mainversion.tar.gz"
+	wget -qO ~/syncthing.tar.gz "$syncthingurl"
     tar xf ~/syncthing.tar.gz
     mv ~/syncthing-linux-amd64-v"$syncthingversion"/syncthing ~/bin/
     cd && rm -rf syncthing{-linux-amd64-v"$syncthingversion",.tar.gz}
     #
     ~/bin/syncthing -generate="~/.config/syncthing"
-    sed -i -r 's#<address>127.0.0.1:(.*)</address>#<address>10.0.0.1:'"$guiport"'</address>#g' ~/.config/syncthing/config.xml
+    sed -i -r 's#<address>127.0.0.1:(.*)</address>#<address>10.0.0.1:'"$appport"'</address>#g' ~/.config/syncthing/config.xml
     sed -i 's#<listenAddress>0.0.0.0:22000</listenAddress>#<listenAddress>0.0.0.0:'"$syncport"'</listenAddress>#g' ~/.config/syncthing/config.xml
     # Apache proxypass
     if [[ ! -f ~/.apache2/conf.d/syncthing.conf ]]
     then
         wget -qO ~/.apache2/conf.d/syncthing.conf "$apacheconf"
-        sed -i -r 's#PORT#'"$guiport"'#g' ~/.apache2/conf.d/syncthing.conf
+        sed -i -r 's#PORT#'"$appport"'#g' ~/.apache2/conf.d/syncthing.conf
         /usr/sbin/apache2ctl -k graceful > /dev/null 2>&1
     else
         configport=$(sed -nr 's#\s*<address>10.0.0.1:(.*)</address>#\1#p' ~/.config/syncthing/config.xml)
@@ -148,7 +303,7 @@ then
         then
             wget -qO ~/.nginx/conf.d/000-default-server.d/syncthing.conf "$nginxconf"
             sed -i -r 's#USERNAME#'"$(whoami)"'#g' ~/.nginx/conf.d/000-default-server.d/syncthing.conf
-            sed -i -r 's#PORT#'"$guiport"'#g' ~/.nginx/conf.d/000-default-server.d/syncthing.conf
+            sed -i -r 's#PORT#'"$appport"'#g' ~/.nginx/conf.d/000-default-server.d/syncthing.conf
             /usr/sbin/nginx -s reload -c ~/.nginx/nginx.conf > /dev/null 2>&1
         else
             configport=$(sed -nr 's#\s*<address>10.0.0.1:(.*)</address>#\1#p' ~/.config/syncthing/config.xml)
@@ -171,7 +326,7 @@ else
     echo -e "You chose to exit after updating the scripts."
     echo
     cd && bash
-    exit 1
+    exit
 fi
 #
 ############################
