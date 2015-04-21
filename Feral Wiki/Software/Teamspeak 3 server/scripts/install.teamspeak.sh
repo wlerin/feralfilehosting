d91ee4f3853b78ca6e1baefbf781c5044abf0b7d
feralhosting <feralhosting@digikool.co.uk>
2015-04-21 08:03:03 +0100

    scripts

--- Feral Wiki/Software/Teamspeak 3 server/scripts/install.teamspeak.sh
+++ Feral Wiki/Software/Teamspeak 3 server/scripts/install.teamspeak.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 #
-# Install Teamspeak 3
-scriptversion="1.1.9"
-teamspeakversion="3.0.11.2"
-scriptname="install.teamspeak"
-# randomessence 27/04/2013
+############################
+##### Basic Info Start #####
+############################
+#
+# Script Author: randomessence
+#
+# Script Contributors: 
+#
+# License: This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License. https://creativecommons.org/licenses/by-sa/4.0/
+#
+# Bash Command for easy reference:
 #
 # wget -qO ~/install.teamspeak http://git.io/aOACkQ && bash ~/install.teamspeak
+#
+############################
+###### Basic Info End ######
+############################
 #
 ############################
 #### Script Notes Start ####
@@ -23,22 +33,25 @@ scriptname="install.teamspeak"
 ############################
 ## Version History Starts ##
 ############################
-# 
-# v 1.0.0 Intial release
-# v 1.0.1 Script
-# v 1.0.2 symlink and code tweaks
-# v 1.0.3 Proper shutdown of any running instances of the server if the ~/private/teamspeak/ does not exists. licensing issues.
-# v 1.0.4 
-# v 1.0.5
-# v 1.0.6
-# v 1.0.7
-# v 1.0.8 used random ports in a range using shuf
-# v 1.1.1 updated to be inline with script formatting guidelines and general tweaks.
-# v 1.1.2 3.0.10.2
-# v 1.1.3 3.0.10.3
-# v 1.1.4 updater tweaked
-# v 1.1.5 template updated
-# v 1.1.7 template updated
+#
+if [[ ! -z $1 && $1 == 'changelog' ]]; then echo
+    #
+    # put your version changes in the single quotes and then uncomment the line.
+    #
+    #echo 'v1.2.0 template updated'
+    #echo 'v1.1.4 updater tweaked'
+    #echo 'v1.1.3 3.0.10.3'
+    #echo 'v1.1.2 3.0.10.2'
+    #echo 'v1.1.1 updated to be inline with script formatting guidelines and general tweaks.'
+    #echo 'v1.0.8 used random ports in a range using shuf'
+    #echo 'v1.0.3 Proper shutdown of any running instances of the server if the ~/private/teamspeak/ does not exists. licensing issues.'
+    #echo 'v1.0.2 symlink and code tweaks'
+    #echo 'v1.0.1 Script'
+    echo 'v1.0.0 Intial release'
+    #
+    echo
+    exit
+fi
 #
 ############################
 ### Version History Ends ###
@@ -48,36 +61,220 @@ scriptname="install.teamspeak"
 ###### Variable Start ######
 ############################
 #
-# Disables the built in script updater permanently.
-updaterenabled="1"
+# Script Version number is set here.
+scriptversion="1.2.0"
 #
+# Script name goes here. Please prefix with install.
+scriptname="install.teamspeak"
+#
+# Author name goes here.
+scriptauthor="None credited"
+#
+# Contributor's names go here.
+contributors="None credited"
+#
+# Set the http://git.io/ shortened URL for the raw github URL here:
+gitiourl="http://git.io/aOACk"
+#
+# Don't edit: This is the bash command shown when using the info option.
+gitiocommand="wget -qO ~/$scriptname $gitiourl && bash ~/$scriptname"
+#
+# This is the raw github url of the script to use with the built in updater.
+scripturl="https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Teamspeak%203%20server/scripts/install.teamspeak.sh"
+#
+# This will generate a 20 character random passsword for use with your applications.
+apppass=$(< /dev/urandom tr -dc '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' | head -c20; echo;)
+# This will generate a random port for the script between the range 10001 to 49999 to use with applications. You can ignore this unless needed.
+appport=$(shuf -i 10001-49999 -n 1)
+#
+# This wil take the previously generated port and test it to make sure it is not in use, generating it again until it has selected an open port.
+while [[ "$(netstat -ln | grep ':'"$appport"'' | grep -c 'LISTEN')" -eq "1" ]]; do appport=$(shuf -i 10001-49999 -n 1); done
+#
+# Script user's http www URL in the format http://username.server.feralhosting.com/
+host1http="http://$(whoami).$(hostname -f)/"
+# Script user's https www URL in the format https://username.server.feralhosting.com/
+host1https="https://$(whoami).$(hostname -f)/"
+# Script user's http www url in the format https://server.feralhosting.com/username/
+host2http="http://$(hostname -f)/$(whoami)/"
+# Script user's https www url in the format https://server.feralhosting.com/username/
+host2https="https://$(hostname -f)/$(whoami)/"
+#
+# feralwww - sets the full path to the default public_html directory if it exists.
+[[ -d ~/www/$(whoami).$(hostname -f)/public_html ]] && feralwww="$HOME/www/$(whoami).$(hostname -f)/public_html/"
+# rtorrentdata - sets the full path to the rtorrent data directory if it exists.
+[[ -d ~/private/rtorrent/data ]] && rtorrentdata="$HOME/private/rtorrent/data"
+# deluge - sets the full path to the deluge data directory if it exists.
+[[ -d ~/private/deluge/data ]] && delugedata="$HOME/private/deluge/data"
+# transmission - sets the full path to the transmission data directory if it exists.
+[[ -d ~/private/transmission/data ]] && transmissiondata="$HOME/private/transmission/data"
+#
+# Bug reporting varaibles.
+makeissue=".makeissue $scriptname A description of the issue"
+ticketurl="https://www.feralhosting.com/manager/tickets/new"
+gitissue="https://github.com/feralhosting/feralfilehosting/issues/new"
+#
+############################
+## Custom Variables Start ##
+############################
+#
+teamspeakversion="3.0.11.2"
+#
+# vport the voice port: random port between 10001-20000 used in the sed commands
 vport=$(shuf -i 10001-20000 -n 1)
-# vport the voice port: random port between 6000-50000 used in the sed commands
-fport=$(shuf -i 20001-35000 -n 1)
+while [[ "$(netstat -ln | grep ':'"$vport"'' | grep -c 'LISTEN')" -eq "1" ]]; do vport=$(shuf -i 10001-20000 -n 1); done
 # fport is file transfer port: vport + 1 used in the sed commands
-qport=$(shuf -i 35001-49999 -n 1)
+fport=$(shuf -i 20001-35000 -n 1)
+while [[ "$(netstat -ln | grep ':'"$fport"'' | grep -c 'LISTEN')" -eq "1" ]]; do fport=$(shuf -i 20001-35000 -n 1); done
 # qport is the query port: vport + 2 used in the sed commands
+qport=$(shuf -i 35001-49999 -n 1)
+while [[ "$(netstat -ln | grep ':'"$qport"'' | grep -c 'LISTEN')" -eq "1" ]]; do qport=$(shuf -i 35001-49999 -n 1); done
+#
 teamspeakfv="http://dl.4players.de/ts/releases/$teamspeakversion/teamspeak3-server_linux-amd64-$teamspeakversion.tar.gz"
 #
-scripturl="https://raw.github.com/feralhosting/feralfilehosting/master/Feral%20Wiki/Software/Teamspeak%203%20server/scripts/install.teamspeak.sh"
+############################
+### Custom Variables End ###
+############################
+#
+# Disables the built in script updater permanently by setting this variable to 0.
+updaterenabled="1"
 #
 ############################
 ####### Variable End #######
 ############################
 #
 ############################
+###### Function Start ######
+############################
+#
+example () {
+    echo "This is my example function"
+}
+#
+############################
+####### Function End #######
+############################
+#
+############################
+#### Script Help Starts ####
+############################
+#
+if [[ ! -z $1 && $1 == 'help' ]]
+then
+    echo
+    echo -e "\033[32m""Script help and usage instructions:""\e[0m"
+    echo
+    #
+    ###################################
+    ##### Custom Help Info Starts #####
+    ###################################
+    #
+    echo -e "Put your help instructions or script guidance here"
+    #
+    ###################################
+    ###### Custom Help Info Ends ######
+    ###################################
+    #
+    echo
+    exit
+fi
+#
+############################
+##### Script Help Ends #####
+############################
+#
+############################
+#### Script Info Starts ####
+############################
+#
+# Use this to show a user script information when they use the info option with the script.
+if [[ ! -z $1 && $1 == 'info' ]]
+then
+    echo
+    echo -e "\033[32m""Script Details:""\e[0m"
+    echo
+    echo "Script version: $scriptversion"
+    echo
+    echo "Script Author: $scriptauthor"
+    echo
+    echo "Script Contributors: $contributors"
+    echo
+    echo -e "\033[32m""Script options:""\e[0m"
+    echo
+    echo -e "\033[36mhelp\e[0m = See the help section for this script."
+    echo
+    echo -e "Example usage: \033[36m$scriptname help\e[0m"
+    echo
+    echo -e "\033[36mchangelog\e[0m = See the version history and change log of this script."
+    echo
+    echo -e "Example usage: \033[36m$scriptname changelog\e[0m"
+    echo
+    echo -e "\033[36minfo\e[0m = Show the script information and usage instructions."
+    echo
+    echo -e "Example usage: \033[36m$scriptname info\e[0m"
+    echo
+    echo -e "\033[31mImportant note:\e[0m Options \033[36mqr\e[0m and \033[36mnu\e[0m are interchangeable and usable together."
+    echo
+    echo -e "For example: \033[36m$scriptname qr nu\e[0m or \033[36m$scriptname nu qr\e[0m will both work"
+    echo
+    echo -e "\033[36mqr\e[0m = Quick Run - use this to bypass the default update prompts and run the main script directly."
+    echo
+    echo -e "Example usage: \033[36m$scriptname qr\e[0m"
+    echo
+    echo -e "\033[36mnu\e[0m = No Update - disable the built in updater. Useful for testing new features or debugging."
+    echo
+    echo -e "Example usage: \033[36m$scriptname nu\e[0m"
+    echo
+    echo -e "\033[32mBash Commands:\e[0m"
+    echo
+    echo -e "\033[36m""$gitiocommand""\e[0m"
+    echo
+    echo -e "\033[36m""~/bin/$scriptname""\e[0m"
+    echo
+    echo -e "\033[36m""$scriptname""\e[0m"
+    echo
+    echo -e "\033[32m""Bug Reporting:""\e[0m"
+    echo
+    echo -e "These are the recommended ways to report bugs for scripts in the FAQs:"
+    echo
+    echo -e "1: In IRC you can use wikibot to create a github issue by using this command format:"
+    echo
+    echo -e "\033[36m""$makeissue""\e[0m"
+    echo
+    echo -e "2: You could open a ticket describing the problem with details of which script and what the problem is."
+    echo
+    echo -e "\033[36m""$ticketurl""\e[0m"
+    echo
+    echo -e "3: You can create an issue directly on github using your github account."
+    echo
+    echo -e "\033[36m""$gitissue""\e[0m"
+    echo
+    echo -e "\033[33m""All bug reports are welcomed and very much appreciated, as they benefit all users.""\033[32m"
+    #
+    echo
+    exit
+fi
+#
+############################
+##### Script Info Ends #####
+############################
+#
+############################
 #### Self Updater Start ####
 ############################
 #
+# Quick Run option part 1: If qr is used it will create this file. Then if the script also updates, which would reset the option, it will then find this file and set it back.
 if [[ ! -z $1 && $1 == 'qr' ]] || [[ ! -z $2 && $2 == 'qr' ]];then echo -n '' > ~/.quickrun; fi
 #
+# No Update option: This disables the updater features if the script option "nu" was used when running the script.
 if [[ ! -z $1 && $1 == 'nu' ]] || [[ ! -z $2 && $2 == 'nu' ]]
 then
     echo
     echo "The Updater has been temporarily disabled"
     echo
-    scriptversion=""$scriptversion"-nu"
+    scriptversion="$scriptversion-nu"
 else
+    #
+    # Check to see if the variable "updaterenabled" is set to 1. If it is set to 0 the script will bypass the built in updater regardless of the options used.
     if [[ "$updaterenabled" -eq 1 ]]
     then
         [[ ! -d ~/bin ]] && mkdir -p ~/bin
@@ -105,10 +302,11 @@ else
         echo
         echo "The Updater has been disabled"
         echo
-        scriptversion=""$scriptversion"-DEV"
+        scriptversion="$scriptversion-DEV"
     fi
 fi
 #
+# Quick Run option part 2: If quick run was set and the updater section completes this will enable quick run again then remove the file.
 if [[ -f ~/.quickrun ]];then updatestatus="y"; rm -f ~/.quickrun; fi
 #
 ############################
@@ -244,6 +442,7 @@ then
 else
     echo -e "You chose to exit after updating the scripts."
     echo
+    cd && bash
     exit
 fi
 #
