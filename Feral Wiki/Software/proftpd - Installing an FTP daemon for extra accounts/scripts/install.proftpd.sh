1231936a2fe99a785d7fdfe3d2e7a5b1a133b81c
feralhosting <feralhosting@digikool.co.uk>
2015-04-28 13:09:51 +0100

    proftpd

--- Feral Wiki/Software/proftpd - Installing an FTP daemon for extra accounts/scripts/install.proftpd.sh
+++ Feral Wiki/Software/proftpd - Installing an FTP daemon for extra accounts/scripts/install.proftpd.sh
@@ -45,6 +45,7 @@ then
     #echo 'v0.0.6 - My changes go here'
     #echo 'v0.0.5 - My changes go here'
     #echo 'v0.0.4 - My changes go here'
+    echo 'v1.2.7 - fixed broken if in adduser section.'
     echo 'v1.2.6 - merged adduser script into main script as a script option and tweaked script.'
     echo 'v1.2.5 - Automatic password generation. USer experience simplified. More useful information is shown.'
     echo 'v1.2.4 - Template updated'
@@ -62,7 +63,7 @@ fi
 ############################
 #
 # Script Version number is set here.
-scriptversion="1.2.6"
+scriptversion="1.2.7"
 #
 # Script name goes here. Please prefix with install.
 scriptname="install.proftpd"
@@ -350,58 +351,57 @@ if [[ -f ~/.quickrun ]];then updatestatus="y"; rm -f ~/.quickrun; fi
 ## Positional Param Start ##
 ############################
 #
-if [[ "$1" = "adduser" ]]
+if [[ ! -z "$1" && "$1" = 'adduser' ]]
 then
     echo
-        #
-        # Edit below this line
-        #
-        # made by finesse for feral hosting FAQ
-        # use as you like
-        # no warranties
-        passwdfile="$HOME/proftpd/etc/ftpd.passwd"
-        groupdfile="$HOME/proftpd/etc/ftpd.group"
-        binarycmd="$HOME/proftpd/bin/ftpasswd"
-        idcount=5001
-        exec 6<&0
-        exec 0<"$passwdfile"
-        while read line1
-        do
-            foundid="$(echo $line1 |grep -o $idcount)"
-            if [ -n "$foundid" ]
-                then
-                    idcount="$(expr $idcount + 1)"
-            fi
-        done
-        exec 0<&6
-        echo -e "Using" "\033[32m""$idcount""\e[0m" "for id's."
-        echo
-        if [ -n "$2" ]
+    #
+    # Edit below this line
+    #
+    # made by finesse for feral hosting FAQ
+    # use as you like
+    # no warranties
+    passwdfile="$HOME/proftpd/etc/ftpd.passwd"
+    groupdfile="$HOME/proftpd/etc/ftpd.group"
+    binarycmd="$HOME/proftpd/bin/ftpasswd"
+    idcount=5001
+    exec 6<&0
+    exec 0<"$passwdfile"
+    while read line1
+    do
+        foundid="$(echo $line1 |grep -o $idcount)"
+        if [ -n "$foundid" ]
             then
-                echo -e "Using ""\033[32m""$2""\e[0m"" for name."
-                name="$2"
-                echo
-            else
-                read -ep "Please input username: " name
-                echo
+                idcount="$(expr $idcount + 1)"
         fi
-        echo -e "\033[32m""Do not include the""\e[0m" "\033[36m""~/""\e[0m" "\033[32m""in the path. Use paths that match existing Jails, relative to your Root directory, for example:""\e[0m"
-        echo
-        echo -e "\033[36m""private/rtorrent/data""\e[0m"
-        echo
-        echo -e "\033[33m""Use TAB to auto complete the path.""\e[0m"
-        echo
-        read -ep "Please specify a relative path to the users home/jail directory: ~/" -i "private/rtorrent/data" jailpath
-        echo
-        echo "$apppass" | "$binarycmd" --passwd --name="$name" --file="$passwdfile" --uid="$idcount" --gid="$idcount" --home="$HOME/$jailpath" --shell="/bin/false" --stdin >/dev/null 2>&1
-        "$binarycmd" --group --name="$name" --file="$groupdfile" --gid="$idcount" --member="$name" >/dev/null 2>&1
-        echo -e "The username is: ""\033[32m""$name""\e[0m"
-        echo
-        echo -e "The password is: ""\033[32m""$apppass""\e[0m"
-        echo
-        echo -e "The jail PATH is: ""\033[36m""$HOME/$jailpath""\e[0m"
-        echo
+    done
+    exec 0<&6
+    echo -e "Using" "\033[32m""$idcount""\e[0m" "for id's."
+    echo
+    if [ -n "$2" ]
+        then
+            echo -e "Using ""\033[32m""$2""\e[0m"" for name."
+            name="$2"
+            echo
+        else
+            read -ep "Please input username: " name
+            echo
     fi
+    echo -e "\033[32m""Do not include the""\e[0m" "\033[36m""~/""\e[0m" "\033[32m""in the path. Use paths that match existing Jails, relative to your Root directory, for example:""\e[0m"
+    echo
+    echo -e "\033[36m""private/rtorrent/data""\e[0m"
+    echo
+    echo -e "\033[33m""Use TAB to auto complete the path.""\e[0m"
+    echo
+    read -ep "Please specify a relative path to the users home/jail directory: ~/" -i "private/rtorrent/data" jailpath
+    echo
+    echo "$apppass" | "$binarycmd" --passwd --name="$name" --file="$passwdfile" --uid="$idcount" --gid="$idcount" --home="$HOME/$jailpath" --shell="/bin/false" --stdin >/dev/null 2>&1
+    "$binarycmd" --group --name="$name" --file="$groupdfile" --gid="$idcount" --member="$name" >/dev/null 2>&1
+    echo -e "The username is: ""\033[32m""$name""\e[0m"
+    echo
+    echo -e "The password is: ""\033[32m""$apppass""\e[0m"
+    echo
+    echo -e "The jail PATH is: ""\033[36m""$HOME/$jailpath""\e[0m"
+    echo
     #
     # Edit above this line
     #
