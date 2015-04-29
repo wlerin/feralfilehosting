29c7a70332a9dc79a503064ba149b47813721010
feralhosting <feralhosting@digikool.co.uk>
2015-04-29 13:31:25 +0100

    profptd

--- Feral Wiki/Software/proftpd - Installing an FTP daemon for extra accounts/scripts/install.proftpd.sh
+++ Feral Wiki/Software/proftpd - Installing an FTP daemon for extra accounts/scripts/install.proftpd.sh
@@ -86,11 +86,11 @@ scripturl="https://raw.githubusercontent.com/feralhosting/feralfilehosting/maste
 #
 # This will generate a 20 character random passsword for use with your applications.
 apppass="$(< /dev/urandom tr -dc '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' | head -c20; echo;)"
-# This will generate a random port for the script between the range 10001 to 49999 to use with applications. You can ignore this unless needed.
-appport="$(shuf -i 10001-49999 -n 1)"
+# This will generate a random port for the script between the range 10001 to 32001 to use with applications. You can ignore this unless needed.
+appport="$(shuf -i 10001-32001 -n 1)"
 #
 # This wil take the previously generated port and test it to make sure it is not in use, generating it again until it has selected an open port.
-while [[ "$(netstat -ln | grep ':'"$appport"'' | grep -c 'LISTEN')" -eq "1" ]]; do appport="$(shuf -i 10001-49999 -n 1)"; done
+while [[ "$(netstat -ln | grep ':'"$appport"'' | grep -c 'LISTEN')" -eq "1" ]]; do appport="$(shuf -i 10001-32001 -n 1)"; done
 #
 # Script user's http www URL in the format http://username.server.feralhosting.com/
 host1http="http://$(whoami).$(hostname -f)/"
@@ -134,15 +134,15 @@ scripturl="https://raw.githubusercontent.com/feralhosting/feralfilehosting/maste
 #
 proftpdurl="ftp://ftp.proftpd.org/distrib/source/proftpd-1.3.5.tar.gz"
 #
-sftpport="$(shuf -i 10001-49999 -n 1)"
+sftpport="$(shuf -i 10001-32001 -n 1)"
 #
 # This wil take the previously generated port and test it to make sure it is not in use, generating it again until it has selected an open port.
-while [[ "$(netstat -ln | grep ':'"$sftpport"'' | grep -c 'LISTEN')" -eq "1" ]]; do sftpport="$(shuf -i 10001-49999 -n 1)"; done
+while [[ "$(netstat -ln | grep ':'"$sftpport"'' | grep -c 'LISTEN')" -eq "1" ]]; do sftpport="$(shuf -i 10001-32001 -n 1)"; done
 #
-ftpsport="$(shuf -i 10001-49999 -n 1)"
+ftpsport="$(shuf -i 10001-32001 -n 1)"
 #
 # This wil take the previously generated port and test it to make sure it is not in use, generating it again until it has selected an open port.
-while [[ "$(netstat -ln | grep ':'"$ftpsport"'' | grep -c 'LISTEN')" -eq "1" ]]; do ftpsport="$(shuf -i 10001-49999 -n 1)"; done
+while [[ "$(netstat -ln | grep ':'"$ftpsport"'' | grep -c 'LISTEN')" -eq "1" ]]; do ftpsport="$(shuf -i 10001-32001 -n 1)"; done
 #
 ############################
 ### Custom Variables End ###
