d5934555bd441830c600f63a93857ea3c67da41e
feralhosting <feralhosting@digikool.co.uk>
2015-04-29 02:50:59 +0100

    dual url

--- Feral Wiki/0 Templates/dualURL/dualurl-1.php
+++ Feral Wiki/0 Templates/dualURL/dualurl-1.php
@@ -1,5 +1,5 @@
 <?php
-# http://git.io/ypa5lA
+# http://git.io/vfNgo
 if ($_SERVER['HTTP_X_HOST'] != $_SERVER['HTTP_HOST']){  // HTTP_HOST is always the same format username.server.feralhosting.com
 $xproxy = '/' . getenv('USER');                         // If the HOSTs don't match then insert this variable
 } else {                                                // If they do match then do this instead
--- Feral Wiki/0 Templates/dualURL/dualurl-2.php
+++ Feral Wiki/0 Templates/dualURL/dualurl-2.php
@@ -1,5 +1,5 @@
 <?php
-# http://git.io/GlFpRQ
+# http://git.io/vfNg1
 if ($_SERVER['HTTP_X_HOST'] != $_SERVER['HTTP_HOST']){  // HTTP_HOST is always the same format username.server.feralhosting.com
 $GLOBALS['xproxy'] = '/' . getenv('USER');              // If the HOSTs don't match then insert this variable
 } else {                                                // If they do match then do this instead
