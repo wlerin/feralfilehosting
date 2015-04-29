465da83d3c4adf1a8a416ae71f6c0fb4fb0948a1
feralhosting <feralhosting@digikool.co.uk>
2015-04-29 16:13:42 +0100

    templates

--- Feral Wiki/0 Templates/Bash Scripts/menu.sh
+++ Feral Wiki/0 Templates/Bash Scripts/menu.sh
@@ -22,71 +22,7 @@
 #### Script Notes Start ####
 ############################
 #
-# This updater deals with updating a single file, the "~/bin/somescript", by updating and switching to this script.
-#
-# So how do I use and customise this updater template with my script?
-#
-### Basic Info section:
-#
-# 1: Optional - fill in the basic info section at the start for those reading the script code.
-# 2: Most of the important information and usage directions are meant to be set in the script info section and used with the script option info. So set them there if required.
-#
-### Version History Section:
-#
-# 3: Modify the version history templates in the Version History section and uncomment on a per line basis to use with the script option "changelog", for example, "somescript changelog"
-#
-### Variables Section:
-#
-# 4: scriptversion="0.0.0" - replace "0.0.0" with your script version. This will be shown to the user at the current version check.
-# 5: scriptname="somescript" - replace "somescript" with your script name. Make it unique to this script. Do not include the file extension.
-# 6: scriptauthor="None credited" - change this to the script author's name.
-# 7: contributors="None credited" - add the names of any contributors you wish to credit here.
-# 8: gitiourl="http://git.io/vvf9K" - change this to the shortened URL provided by http://git.io once you have committed the script to github or from a gist.
-# 9: gitiocommand="wget -qO ~/$scriptname $gitiourl && bash ~/$scriptname" - Leave this as it is. Do not modify it. This variable depends on the correct setting of the $gitiourl variable.
-# 10: scripturl="https://raw.github.com/feralhosting" - Set the scripturl variable in the variable section to the RAW github URL of the script for use with the updater features.
-# 11: appport=$(shuf -i 10001-49999 -n 1) - works in tandem with a while loop just below it. Will generate and test a port to make sure it is not in use. Use this when configuring an application's port.
-# 12: updaterenabled="1" - Set this to 0 to permanently disable the built in updater and associated features.
-#
-### Variables Section: Custom Variables
-#
-# 13: Place your custom variables here. They can be used in the help option of the script.
-#
-# Menu Template specific: options1,options2,options3 are the option descriptions in the script. If you add more option you also need to modify the menu entry in the script to match.
-# See option4,option5,option6 as examples to modify.
-#
-### Functions Section:
-#
-# 14: Place your custom functions here. Your functions will be able to make use of variables sections in this location. There is an example function in this section.
-#
-### Script Help Section:
-#
-# 15: Place your help information and usage instructions inside the section labelled "Custom Script Notes" using echoes.
-#
-### Script Info Section:
-#
-# Important note: The info section contains basic information about the features of this updater and the script. Don't modify these. You have a section dedicated to your unique information.
-#
-# 16: Place your unique information and usage instructions inside the section labelled "Custom Script Notes" using echoes.
-#
-### Self Updater Section:
-#
-# 17: This section is self contained you don't need to modify this section. This feature will compare itself vs the raw script linked at the github URL provided and update itself
-#
-### User Scripts:
-#
-# Important Note: This template is a wrapper around your script. You will need to make use of the script option below like qr to call your own options.
-#
-# 18: Insert your script in the "User Script" labelled section - Indented by one tab (4 spaces) to be in line with the overall script. You can copy and paste a working script into this section.
-#
-### Script Options explained:
-#
-# 19: changelog - use the argument qr when calling the script, for example - "somescript changelog".
-# 20: help - Use this section to create help notes and usage instructions for the user when they use this option, for example - "somescript help".
-# 21: info - use the argument qr when calling the script, for example - "somescript info".
-# 22: qr - use this option to quick run the script suppressing all update prompts and jumping directly to the user script, for example - "somescript qr". Note - This does not disable or bypass the updater.
-# 23: nu - use the option to disable the update features of the script, for example - "somescript nu". Note - This will run the script from where it is called and append -DEV to the version number output.
-#
-# 24: To pass your own variables to the script in the user script section please start from $2 onwards.
+## See readme.md
 #
 ############################
 ##### Script Notes End #####
@@ -144,15 +80,15 @@ gitiourl="http://git.io/vvf9K"
 gitiocommand="wget -qO ~/$scriptname $gitiourl && bash ~/$scriptname"
 #
 # This is the raw github url of the script to use with the built in updater.
-scripturl="https://raw.github.com/feralhosting/feralfilehosting/master/"
+scripturl="https://raw.githubusercontent.com/feralhosting/feralfilehosting/master/Feral%20Wiki/0%20Templates/Bash%20Scripts/menu.sh"
 #
 # This will generate a 20 character random passsword for use with your applications.
 apppass="$(< /dev/urandom tr -dc '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' | head -c20; echo;)"
-# This will generate a random port for the script between the range 10001 to 49999 to use with applications. You can ignore this unless needed.
-appport="$(shuf -i 10001-49999 -n 1)"
+# This will generate a random port for the script between the range 10001 to 32001 to use with applications. You can ignore this unless needed.
+appport="$(shuf -i 10001-32001 -n 1)"
 #
 # This wil take the previously generated port and test it to make sure it is not in use, generating it again until it has selected an open port.
-while [[ "$(netstat -ln | grep ':'"$appport"'' | grep -c 'LISTEN')" -eq "1" ]]; do appport="$(shuf -i 10001-49999 -n 1)"; done
+while [[ "$(netstat -ln | grep ':'"$appport"'' | grep -c 'LISTEN')" -eq "1" ]]; do appport="$(shuf -i 10001-32001 -n 1)"; done
 #
 # Script user's http www URL in the format http://username.server.feralhosting.com/
 host1http="http://$(whoami).$(hostname -f)/"
@@ -330,8 +266,10 @@ fi
 #### Self Updater Start ####
 ############################
 #
+# Checks for the positional parameters $1 and $2 to be reset if the script is updated.
+[[ ! -z "$1" && "$1" != 'qr' ]] || [[ ! -z "$2" && "$2" != 'qr' ]] && echo -en "$1\n$2" > ~/.passparams
 # Quick Run option part 1: If qr is used it will create this file. Then if the script also updates, which would reset the option, it will then find this file and set it back.
-if [[ ! -z "$1" && "$1" = 'qr' ]] || [[ ! -z "$2" && "$2" = 'qr' ]];then echo -n '' > ~/.quickrun; fi
+[[ ! -z "$1" && "$1" = 'qr' ]] || [[ ! -z "$2" && "$2" = 'qr' ]] && echo -n '' > ~/.quickrun
 #
 # No Update option: This disables the updater features if the script option "nu" was used when running the script.
 if [[ ! -z "$1" && "$1" = 'nu' ]] || [[ ! -z "$2" && "$2" = 'nu' ]]
@@ -375,10 +313,40 @@ else
 fi
 #
 # Quick Run option part 2: If quick run was set and the updater section completes this will enable quick run again then remove the file.
-if [[ -f ~/.quickrun ]];then updatestatus="y"; rm -f ~/.quickrun; fi
+[[ -f ~/.quickrun ]] && updatestatus="y"; rm -f ~/.quickrun
+#
+# resets the positional parameters $1 and $2 post update.
+[[ -f ~/.passparams ]] && set "$1" "$(sed -n '1p' ~/.passparams)" && set "$2" "$(sed -n '2p' ~/.passparams)"; rm -f ~/.passparams
 #
 ############################
 ##### Self Updater End #####
+############################
+#
+############################
+## Positional Param Start ##
+############################
+#
+if [[ ! -z "$1" && "$1" = "example" ]]
+then
+    echo
+    #
+    # Edit below this line
+    #
+    echo "Add your custom positional parameters in this section."
+    #
+    if [[ -n "$2" ]]
+    then
+        echo "You used $scriptname $1 $2 when calling this example"
+    fi
+    #
+    # Edit above this line
+    #
+    echo
+    exit
+fi
+#
+############################
+### Positional Param End ###
 ############################
 #
 ############################
